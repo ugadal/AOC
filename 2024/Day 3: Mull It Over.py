d657e95bdc06150a68de65f32a6c4781cccdba78
Ugadal <dcoorna@gmail.com>
2024-12-03 16:43:14 +0100

    up

--- 2024/Day 3: Mull It Over.py
+++ 2024/Day 3: Mull It Over.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 #
-fn,part="d2.txt",1
-data=open(fn).read().split("\n\n")[part].splitlines()
+fn,part="d3.txt",1
+data=open(fn).read().split("\n\n")[part]
+print(data)
+import re
+er=re.compile("mul\\((\\d+,\\d+)\\)")
+res=0
+for block in er.findall(data):
+	a,b=map(int,block.split(","))
+	res+=a*b
+print(res)
