92f8a599382e60ea0f4bffbe665008934f4d49a1
David Coornaert <dcoorna@gmail.com>
2024-12-04 08:34:21 +0100

    up

---
+++ 2024/tmp/Day 4: Ceres Search.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+fn,part="d4.txt",0
+G=open(fn).read().split("\n\n")[part].splitlines()
+NR=len(G)
+NC=len(G[0])
+border="."*(NC+6)
+NG=[border,border,border]
+for line in G:NG.append("..."+line+"...")
+for x in range(3):NG.append(border)
+G=NG
+def valid(pos,dirs):
+	r,c=pos
+	dr,dc=dirs
+	for (a,b),s in zip([(r+k*dr,c+k*dc) for k in range(1,4)],"MAS"):
+		if G[a][b]!=s:return False
+	return True
+for l in G:print(l)
+import itertools as it
+dirs=list(it.product((-1,1,0),repeat=2))[:-1]
+res=0
+for row in range(3,NR+3):
+	for col in range(3,NC+3):
+		if G[row][col]!="X":continue
+		pos=(row,col)
+		for d in dirs:
+			if valid(pos,d):res+=1
+print(res)
+dirs=[a+b for a,b in it.product((-1,1,0),(-1j,1j,0))][:-1]
+for d in dirs:print(d)
