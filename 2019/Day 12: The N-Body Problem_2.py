f9d5789cc7917f0f5553e6197538deb1e2e1ce35
Ugadal <dcoorna@gmail.com>
2024-12-08 18:06:57 +0100

    up

--- 2019/Day 12: The N-Body Problem_2.py
+++ 2019/Day 12: The N-Body Problem_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-fn,part="d12.txt",2
+fn,part="d12.txt",3
 sep="\n\n"
 data=open(fn).read().split(sep)[part].splitlines()
 class moon():
@@ -27,12 +27,13 @@ class moon():
 		self.E=pot*kin
 Moons=[moon(line) for line in data]
 cyc=0
-V={}
-hashmoons="".join([m.__str__() for m in Moons])
-tte=sum(m.E for m in Moons)
-V[tte]=[hashmoons]
-
-
+# ~ V={}
+# ~ hashmoons="".join([m.__str__() for m in Moons])
+# ~ tte=sum(m.E for m in Moons)
+# ~ V[tte]=[hashmoons]
+moona=Moons[0]
+V=[moona.__str__()]
+pcyc=0
 while True:
 	cyc+=1
 	for pa,ma in enumerate(Moons[:-1]):
@@ -57,13 +58,25 @@ while True:
 				mb.dz+=1
 	# ~ for moon in Moons:print(moon)
 	for moon in Moons:moon.move()
+	if moona.E==0:
+		print(cyc,cyc-pcyc)
+		pcyc=cyc
+		# ~ exit()
 # ~ for moon in Moons:print(moon)
-	tte=sum(m.E for m in Moons)
-	if cyc%100000==0:print(cyc,tte)
-	if tte not in V:V[tte]=[]
-	hashmoons="".join([m.__str__() for m in Moons])
-	if hashmoons in V[tte]:
-		print(cyc)
-		for moon in Moons:print(moon)
-		exit()
-	V[tte].append(hashmoons)
+	# ~ tte=sum(m.E for m in Moons)
+	# ~ if cyc%100000==0:print(cyc,tte)
+	# ~ print(cyc,tte)
+	# ~ if tte not in V:V[tte]=[]
+	# ~ else:print("known energy")
+	# ~ hashmoons="".join([m.__str__() for m in Moons])
+	# ~ if hashmoons in V[tte]:
+		# ~ print(cyc)
+		# ~ for moon in Moons:print(moon)
+		# ~ exit()
+	# ~ V[tte].append(hashmoons)
+	# ~ scm=moona.__str__()
+	# ~ if scm in V:
+		# ~ print(scm,cyc,V.index(scm))
+		# ~ exit()
+	# ~ else:V.append(scm)
+	# ~ print(len(V))
