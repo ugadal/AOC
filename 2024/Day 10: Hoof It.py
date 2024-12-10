5c197c95290ac947a7de27472252a7db9426b752
Ugadal <dcoorna@gmail.com>
2024-12-10 08:24:25 +0100

    up

--- 2024/Day 10: Hoof It.py
+++ 2024/Day 10: Hoof It.py
@@ -3,7 +3,6 @@
 sep="\n\n"
 fn,part="d10.txt",1
 data=open(fn).read().split(sep)[part].splitlines()
-# ~ the sum of the scores of all trailheads is 36
 G={}
 D=[-1j,1,1j,-1]
 for row, line in enumerate(data):
@@ -11,7 +10,6 @@ for row, line in enumerate(data):
 		G[col+row*1j]=int(v)
 zeroes=[k for k,v in G.items() if v==0]
 def reachable(cp,cv=0):
-	# ~ print(" "*cv,f"entered pos {cp} v {cv}")
 	if cv==9:
 		yield cp
 	else:
@@ -23,8 +21,5 @@ def countreached(sp):
 	return len(set(R))
 r=sum(countreached(zero) for zero in zeroes)
 print(r)
-# ~ print(zeroes[0])
-# ~ for v in reachable(zeroes[0]):print(v)
-# ~ R=list(reachable(zeroes[0]))
-# ~ print(R)
-# ~ print(len(set(R)))
+r=sum(len(list(reachable(zero))) for zero in zeroes)
+print(r)
