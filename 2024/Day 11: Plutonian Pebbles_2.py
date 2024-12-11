c195722c8f6cd3ef2b9ea436f032dccb425387b0
Ugadal <dcoorna@gmail.com>
2024-12-11 08:19:11 +0100

    up

--- 2024/Day 11: Plutonian Pebbles_2.py
+++ 2024/Day 11: Plutonian Pebbles_2.py
@@ -1,31 +1,34 @@
 #!/usr/bin/env python3
 #
 from collections import deque
+from functools import wraps
+def memoize(func):
+    cache = {}
+    @wraps(func)
+    def wrapper(*args):
+        if args in cache:
+            return cache[args]
+        else:
+            result = func(*args)
+            cache[args] = result
+            return result
+    return wrapper
+
 sep="\n\n"
 fn,part="d11.txt",0
-# ~ data=open(fn).read().split(sep)[part].splitlines()
-# ~ data=deque(open(fn).readline().split())
 data=open(fn).readline().split()
-data=[(x,32) for x in data]
-
-def blink(v):
-	if v=="0":return ["1"]
-	lv=len(v)
-	if lv%2==0:return[str(int(v[:lv//2])),str(int(v[lv//2:]))]
-	return [str(int(v)*2024)]
-# ~ data=["125","17"]
-# ~ data=[(x,25) for x in data]
-data=deque(data)
-count=0
-# ~ out=deque()
-while data:
-	v,bn=data.pop()
-	# ~ print(v,bn)
-	if bn==0:
-		# ~ out.appendleft(v)
-		count+=1
+@memoize
+def blink(v,bn):
+	if bn==1:
+		if v=="0":return 1
+		lv=len(v)
+		if lv%2:return 1
+		return 2
 	else:
-		for nv in blink(v):
-			data.append((nv,bn-1))
-# ~ print(count,out)
-print(count)
+		if v=="0":return blink("1",bn-1)
+		lv=len(v)
+		if lv%2==0:return blink(str(int(v[:lv//2])),bn-1)+blink(str(int(v[lv//2:])),bn-1)
+		return blink(str(int(v)*2024),bn-1)
+print(blink("125",4))
+print(blink("17",4))
+print(sum(blink(x,75) for x in data))
