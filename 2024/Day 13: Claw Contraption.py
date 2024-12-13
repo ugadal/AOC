dd8afa6708db7043de636fdfa0fd023133b22b28
Ugadal <dcoorna@gmail.com>
2024-12-13 17:58:48 +0100

    up

--- 2024/Day 13: Claw Contraption.py
+++ 2024/Day 13: Claw Contraption.py
@@ -8,14 +8,13 @@ import re
 bp=re.compile(r"Button\s\S: X\+(\d+), Y\+(\d+)")
 pp=re.compile(r"Prize:\sX=(\d+),\sY=(\d+)")
 def treat(a,b,c,d,e,f):
-	c+=10000000000000
-	f+=10000000000000
+	c+=10000000000000 #part2
+	f+=10000000000000 #part2
 	y=(a*f-d*c)/(a*e-d*b)
 	x=(c-b*y)/a
-	if int(x)==x and int(y)==y:return x,y
+	if x.is_integer() and y.is_integer():return x,y
 	return None,None
 cost=0
-# ~ for block in data.split(sep)[:-1]:
 for block in data.split(sep):
 	la,lb,lc=block.splitlines()
 	a,d=bp.findall(la)[0]
@@ -23,6 +22,5 @@ for block in data.split(sep):
 	c,f=pp.findall(lc)[0]
 	a,b,c,d,e,f=map(int,(a,b,c,d,e,f))
 	x,y=treat(a,b,c,d,e,f)
-	if x:
-		cost+=3*x+y
+	if x:cost+=3*x+y
 print(int(cost))
