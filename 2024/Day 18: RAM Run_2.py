bd688bc814010308d6e660a9f945c966afd8cfdb
Ugadal <dcoorna@gmail.com>
2024-12-18 11:41:46 +0100

    up

--- 2024/Day 18: RAM Run_2.py
+++ 2024/Day 18: RAM Run_2.py
@@ -28,7 +28,7 @@ def around(p):
 def draw():
 	for row in range(NR):
 		print( "".join( [ G.get(col+row*1j,"#") for col in range(NC) ] ) )
-draw()
+# ~ draw()
 def seekpath():
 	shortest=float("Inf")
 	V=set()
@@ -46,7 +46,6 @@ def seekpath():
 		for np in around(cpos):
 			if G.get(np,"#")==".":todo.append((np,l+1,path+[cpos]))
 		V.add(cpos)
-		# ~ print("V",V)
 	return shortest,bp
 r,bp=seekpath()	
 for rock in rocks[1024:]:
@@ -57,31 +56,3 @@ for rock in rocks[1024:]:
 		print (rock,d)
 		break
 	else:print(rock,d)
-# ~ draw()
-exit()
-sp=next(p for p,v in G.items() if v=="S")
-ep=next(p for p,v in G.items() if v=="E")
-print(sp,ep)
-cpos=sp
-sco=0
-V={}
-todo=[(sp,1,0,[])]
-record=float("inf")
-inpath=set()
-while todo:
-	cp,cd,cs,path=todo.pop(0)
-	# ~ print(f"pos {cp}, dir {cd}, cs {cs}")
-	if cs>record:continue
-	if (cp,cd) in V and V[(cp,cd)]<cs:continue
-	if cp==ep:
-		if cs<record:
-			inpath=set()
-			record=cs
-		if cs==record:
-			for p in path:inpath.add(p)
-			continue
-	if G[cp+cd]!="#":todo.append((cp+cd,cd,cs+1,path+[cp]))
-	todo.append((cp,cd*1j,cs+1000,path))
-	todo.append((cp,cd*(-1j),cs+1000,path))
-	V[(cp,cd)]=cs
-print(record,len(inpath)+1)
---
+++ 2024/HNday18p2.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+s = 70
+
+coords = [list(map(int, line.split(","))) for line in open(0)]
+
+def connected(n):
+    grid = [[0] * (s + 1) for _ in range(s + 1)]
+
+    for c, r in coords[:n]:
+        grid[r][c] = 1
+
+    q = deque([(0, 0)])
+    seen = {(0, 0)}
+
+    while q:
+        r, c = q.popleft()
+        for nr, nc in [(r + 1, c), (r, c + 1), (r - 1, c), (r, c - 1)]:
+            if nr < 0 or nc < 0 or nr > s or nc > s: continue
+            if grid[nr][nc] == 1: continue
+            if (nr, nc) in seen: continue
+            if nr == nc == s: return True
+            seen.add((nr, nc))
+            q.append((nr, nc))
+
+    return False
+
+lo = 0
+hi = len(coords) - 1
+ld=0
+while lo < hi:
+    ld+=1
+    mi = (lo + hi) // 2
+    if connected(mi + 1):
+        lo = mi + 1
+    else:
+        hi = mi
+
+print(*coords[lo], sep=",")
+print(ld)
