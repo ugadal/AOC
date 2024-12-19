996ee514fed11cb81d2598277c943776b3b2fd30
Ugadal <dcoorna@gmail.com>
2024-12-19 06:39:49 +0100

    up

--- 2024/Day 19: Linen Layout_2.py
+++ 2024/Day 19: Linen Layout_2.py
@@ -6,21 +6,8 @@ sep="\n\n"
 fn,part="d19.txt",2
 parts=open(fn).read().split(sep)[part].split(', ')
 wanted=open(fn).read().split(sep)[part+1].splitlines()
-print( parts)
-parts.sort()
-parts.reverse()
-print(wanted)
 @cache
-def possible(s):
-	target=s
-	# ~ print(target)
+def possible(target):
 	if not target:return 1
 	return sum(possible(target[len(p):]) for p in parts if target.startswith(p))
-	return False
-cc=0				
-for s in wanted:
-	print(s)
-	w=possible(s)
-	print(s,w)
-	cc+=w
-print(cc)
+print (sum(possible(s) for s in wanted))
