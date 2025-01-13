8f607456c034602a4e5eb2c099d802d8f60d800d
Ugadal <dcoorna@gmail.com>
2025-01-13 17:44:32 +0100

    up

--- 2024/Day 23: LAN Party_2.py
+++ 2024/Day 23: LAN Party_2.py
@@ -5,7 +5,7 @@ from collections import deque
 from functools import cache
 import itertools as it
 sep="\n\n"
-fn,part="d23.txt",0
+fn,part="d23.txt",1
 pcnames=set()
 Dpc={}
 class pc():
@@ -26,30 +26,22 @@ t=0
 def allin(g):
 	return all(a in b.partners for a,b in it.combinations(g,2))
 z=3
-Pool=set(Dpc.values())
-# ~ record=float("-Inf")
-# ~ for a,b in it.combinations(Pool,2):
-	# ~ if not a in b.partners:continue
-	
-	# ~ p=a.partners&b.partners
-	# ~ NP=Pool
-	# ~ for x in p:
-		# ~ Pool=Pool&x.partners
-	# ~ if Pool!=p:continue
-	
-	# ~ lp=len(p)
-	# ~ if lp>record:
-		# ~ record=lp
-		# ~ N=[x.name for x in p]
-		# ~ N.sort()
-		# ~ print(a.name,b.name,lp,",".join(N))
-# ~ exit()
-# ~ Npool=set()
-for n,g in enumerate(it.combinations(Pool,z)):
-	if not any(x.good for x in g):continue
-	if allin(g):
-		# ~ print([x.name for x in g])
-	# ~ Npool.add(g)
-		t+=1
-print(t)
-# ~ print(len(Npool))
+# ~ Pool=set(Dpc.values())
+Pool=list(Dpc.values())
+PL=[]
+while True:
+	for p in Pool:
+		for pi,pl in enumerate(PL):
+			if allin(pl|set([p])):PL[pi]=pl|set([p])
+		PL.append(set([p]))
+	# ~ print([len(x) for x in PL])
+	mx=max([len(x) for x in PL])
+	print(mx)
+	S=set()
+	for pl in PL:
+		if len(pl)==mx:
+			N=[x.name for x in pl]
+			N.sort()
+			S.add(",".join(N))
+	for s in S:print(s)
+	input()
