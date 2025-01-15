9cc4f2af2fa80cc0dfb8c006393ba74c77156f70
Ugadal <dcoorna@gmail.com>
2025-01-15 17:51:47 +0100

    up

--- 2024/Day 25: Code Chronicle.py
+++ 2024/Day 25: Code Chronicle.py
@@ -2,7 +2,6 @@
 #
 # ~ from functools import cache
 from collections import deque
-from functools import cache
 import itertools as it
 sep="\n\n"
 fn,part="d25.txt",0
@@ -17,8 +16,5 @@ res=0
 for k,l in it.product(K,L):
 	for a,b in zip(k,l):
 		if a==b=="#":break
-	else:
-		print(k)
-		print(l)
-		res+=1
+	else:res+=1
 print(res)
