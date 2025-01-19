2a4fe9f32066c9c19b1fa28f284f7c38de450449
David Coornaert <dcoorna@gmail.com>
2025-01-19 21:39:44 +0100

    up

--- EC/2024/Quest 2: The Runes of Power.py
+++ EC/2024/Quest 2: The Runes of Power.py
@@ -11,3 +11,13 @@ tt=0
 for r in R:
 	tt+=len(r.findall(txt))
 print(tt)
+fn="everybody_codes_e2024_q02_p2.txt"
+wd,txt=open(fn).read().split("\n\n")
+print(wd)
+wd=wd.split(":")[1].split(",")
+print(wd)
+tt=0
+print(txt)
+R=[re.compile(s) for s in wd]
+for r in R:
+	tt+=print(r.findall(txt))
