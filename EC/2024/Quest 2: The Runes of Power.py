5b6bfe3c314964702c6c8ad7c8b8c9e2d60b4f7d
David Coornaert <dcoorna@gmail.com>
2025-01-20 13:59:38 +0100

    up

--- EC/2024/Quest 2: The Runes of Power.py
+++ EC/2024/Quest 2: The Runes of Power.py
@@ -3,22 +3,16 @@
 import re
 fn="everybody_codes_e2024_q02_p1.txt"
 wd,txt=open(fn).read().split("\n\n")
-# ~ print(wd)
 wd=wd.split(":")[1].split(",")
-# ~ print(wd)
 R=[re.compile(s) for s in wd]
 tt=0
 for r in R:
 	tt+=len(r.findall(txt))
-# ~ print(tt)
+# ~ P2
 fn="everybody_codes_e2024_q02_p2.txt"
-# ~ fn="t.txt"
 wd,txt=open(fn).read().split("\n\n")
-# ~ print(wd)
 wd=wd.split(":")[1].split(",")
-# ~ print(wd)
 tt=0
-# ~ print(txt)
 R=[]
 for s in wd:
 	R.append(re.compile(s))
@@ -26,19 +20,46 @@ for s in wd:
 	t.reverse()
 	t="".join(t)
 	R.append(re.compile(t))
-# ~ R=[re.compile(s) for s in wd]
 for l in txt.splitlines():
-	# ~ print(l)
 	s=set()
 	for r in R:
-		# ~ print(r)
 		pos=0
 		sr=r.search(l,pos)
 		while sr:
-			# ~ print(sr)
 			for x in range (sr.start(),sr.end()):s.add(x)
 			pos=sr.start()+1
 			sr=r.search(l,pos)
-	# ~ print(s,len(s))
 	tt+=len(s)
 print(tt)
+# ~ P3
+fn="everybody_codes_e2024_q02_p3.txt"
+fn="t.txt"
+wd,txt=open(fn).read().split("\n\n")
+wd=wd.split(":")[1].split(",")
+R=[]
+for s in wd:
+	R.append(re.compile(s))
+	t=list(s)
+	t.reverse()
+	t="".join(t)
+	R.append(re.compile(t))
+txt=txt.splitlines()
+NR=len(txt)
+NC=len(txt[0])
+tt=0
+for l in txt:
+	s=set()
+	print(l)
+	for r in R:
+		print(r)
+		pos=0
+		tl=l+l[:len(r.pattern)-1]
+		sr=r.search(tl,pos)
+		while sr:
+			print(sr)
+			for x in range (sr.start(),sr.end()):s.add(x)
+			pos=sr.start()+1
+			sr=r.search(tl,pos)
+	tt+=len(s)
+for r in txt:print(r)
+txt=list(map(lambda x:"".join(x),zip(*txt)))
