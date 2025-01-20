610ac96cb487029237bb514668ebe4043380b279
David Coornaert <dcoorna@gmail.com>
2025-01-20 11:14:52 +0100

    up

--- EC/2024/Quest 2: The Runes of Power.py
+++ EC/2024/Quest 2: The Runes of Power.py
@@ -3,22 +3,22 @@
 import re
 fn="everybody_codes_e2024_q02_p1.txt"
 wd,txt=open(fn).read().split("\n\n")
-print(wd)
+# ~ print(wd)
 wd=wd.split(":")[1].split(",")
-print(wd)
+# ~ print(wd)
 R=[re.compile(s) for s in wd]
 tt=0
 for r in R:
 	tt+=len(r.findall(txt))
-print(tt)
+# ~ print(tt)
 fn="everybody_codes_e2024_q02_p2.txt"
 # ~ fn="t.txt"
 wd,txt=open(fn).read().split("\n\n")
-print(wd)
+# ~ print(wd)
 wd=wd.split(":")[1].split(",")
-print(wd)
+# ~ print(wd)
 tt=0
-print(txt)
+# ~ print(txt)
 R=[]
 for s in wd:
 	R.append(re.compile(s))
@@ -28,17 +28,17 @@ for s in wd:
 	R.append(re.compile(t))
 # ~ R=[re.compile(s) for s in wd]
 for l in txt.splitlines():
-	print(l)
+	# ~ print(l)
 	s=set()
 	for r in R:
-		print(r)
+		# ~ print(r)
 		pos=0
 		sr=r.search(l,pos)
 		while sr:
-			print(sr)
+			# ~ print(sr)
 			for x in range (sr.start(),sr.end()):s.add(x)
 			pos=sr.start()+1
 			sr=r.search(l,pos)
-	print(s,len(s))
+	# ~ print(s,len(s))
 	tt+=len(s)
 print(tt)
