ef9a88b698a89247af74e444c6b30ddeb1084024
Ugadal <dcoorna@gmail.com>
2025-01-21 09:43:56 +0100

    up

--- EC/2024/Quest 3: Mining Maestro.py
+++ EC/2024/Quest 3: Mining Maestro.py
@@ -46,5 +46,65 @@ rep()
 print(sum(v for k,v in G.items() if v !="."))
 print("part 2".center(60).title())
 fn="everybody_codes_e2024_q03_p2.txt"
+G={}
+for r,row in enumerate(open(fn).read().splitlines()):
+	for c,s in enumerate(row):
+		t=complex(c,r)
+		G[t]=s
+NR=r+1
+NC=c+1
+for k,v in G.items():
+	if v=="#":G[k]=1
+	# ~ if v==".":G[k]=-1
+rep()
+lvl=1
+while True:
+	todo=[]
+	for k,v in G.items():
+		if v!=lvl:	continue
+		if all((G.get(p,-5)==lvl for p in around(k))):
+			todo.append(k)
+	if todo:
+		for k in todo:G[k]=lvl+1
+		lvl+=1
+	else:break
+print(todo,lvl)
+rep()
+print(sum(v for k,v in G.items() if v !="."))
 print("part 3".center(60).title())
 fn="everybody_codes_e2024_q03_p3.txt"
+# ~ fn="t.txt"
+G={}
+for r,row in enumerate(open(fn).read().splitlines()):
+	for c,s in enumerate(row):
+		t=complex(c,r)
+		G[t]=s
+NR=r+1
+NC=c+1
+for k,v in G.items():
+	if v=="#":G[k]=1
+	# ~ if v==".":G[k]=-1
+def around(p):
+	yield p+1
+	yield p-1
+	yield p+1j
+	yield p-1j
+	yield p-1-1j
+	yield p-1+1j
+	yield p+1-1j
+	yield p+1+1j
+rep()
+lvl=1
+while True:
+	todo=[]
+	for k,v in G.items():
+		if v!=lvl:	continue
+		if all((G.get(p,-5)==lvl for p in around(k))):
+			todo.append(k)
+	if todo:
+		for k in todo:G[k]=lvl+1
+		lvl+=1
+	else:break
+print(todo,lvl)
+rep()
+print(sum(v for k,v in G.items() if v !="."))
