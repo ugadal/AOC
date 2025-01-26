1c1c56f5232afa347f19d87b3945e5a63dea4540
Ugadal <dcoorna@gmail.com>
2025-01-26 10:46:57 +0100

    up

--- 2019/Day 14: Space Stoichiometry.py
+++ 2019/Day 14: Space Stoichiometry.py
@@ -26,45 +26,20 @@ ore={}
 done={}
 used={}
 while True:
-	target=nxtodo()
+	try:target=nxtodo()
+	except:break
 	needed=todo[target]-done.get(target,0)+used.get(target,0)
-	print("needed",needed,"of",target)
+	# ~ print("needed",needed,"of",target)
 	doneonce,parts=rules[target]
-	print("pack:",doneonce,", recipe:",parts)
+	# ~ print("pack:",doneonce,", recipe:",parts)
 	runs=math.ceil(needed/doneonce)
-	print("runs required",runs)
+	# ~ print("runs required",runs)
 	done[target]=done.get(target,0)+runs*doneonce
 	used[target]=used.get(target,0)+todo[target]
-	print("done",done)
-	print("used",used)
+	# ~ print("done",done)
+	# ~ print("used",used)
 	for k,v in parts.items():
 		todo[k]=todo.get(k,0)+v*runs
 	todo[target]=0
-	print("todo",todo)
-	# ~ exit()
-	# ~ qt,t=todo.pop(0)# 1 fuel
-	# ~ todo,rec=rules[t] #td:1   rec:{a:7 , e:1]
-	
-	# ~ if "ORE" in rec:
-		# ~ ore[t]=ore.get(t,0)+qt
-		# ~ continue
-	# ~ this={}
-	# ~ while qt>0:
-		# ~ for x,y in rec.items():
-			# ~ this[x]=this.get(x,0)+y
-		# ~ qt-=td
-	# ~ print(this)
-	# ~ for k,v in this.items():todo.append((v,k))
 	# ~ print("todo",todo)
-	# ~ print(ore)
-	# ~ input()
-print(ore)
-tt=0
-for k,needed in ore.items():
-	print(k,needed)
-	done,od=rules[k]
-	print(done,od)
-	while needed>0:
-		tt+=od["ORE"]
-		needed-=done
-print(tt)
+print(todo["ORE"])
