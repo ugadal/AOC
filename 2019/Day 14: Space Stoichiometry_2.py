644a971bca329efeb4f30c18cac21a7eff22b4fa
Ugadal <dcoorna@gmail.com>
2025-01-26 11:02:22 +0100

    up

--- 2019/Day 14: Space Stoichiometry_2.py
+++ 2019/Day 14: Space Stoichiometry_2.py
@@ -18,28 +18,32 @@ for l in data.splitlines():
 		x,y=p.split()
 		I[y]=int(x)
 	rules[t]=(int(q),I)
-# ~ print(rules)
-def nxtodo():
+print(rules)
+def nxtodo(todo):
 	return next(k for k,v in todo.items() if v>0 and k!="ORE")
-todo={"FUEL":6326857}
-ore={}
-done={}
-used={}
+def seek(x):
+	todo={"FUEL":x}
+	ore={}
+	done={}
+	used={}
+	while True:
+		try:target=nxtodo(todo)
+		except:break
+		needed=todo[target]-done.get(target,0)+used.get(target,0)
+		doneonce,parts=rules[target]
+		runs=math.ceil(needed/doneonce)
+		done[target]=done.get(target,0)+runs*doneonce
+		used[target]=used.get(target,0)+todo[target]
+		for k,v in parts.items():
+			todo[k]=todo.get(k,0)+v*runs
+		todo[target]=0
+		# ~ print(todo)
+	return todo["ORE"]<=1000000000000
+lo=0
+hi=100000000
 while True:
-	try:target=nxtodo()
-	except:break
-	needed=todo[target]-done.get(target,0)+used.get(target,0)
-	# ~ print("needed",needed,"of",target)
-	doneonce,parts=rules[target]
-	# ~ print("pack:",doneonce,", recipe:",parts)
-	runs=math.ceil(needed/doneonce)
-	# ~ print("runs required",runs)
-	done[target]=done.get(target,0)+runs*doneonce
-	used[target]=used.get(target,0)+todo[target]
-	# ~ print("done",done)
-	# ~ print("used",used)
-	for k,v in parts.items():
-		todo[k]=todo.get(k,0)+v*runs
-	todo[target]=0
-	# ~ print("todo",todo)
-print(todo["ORE"]<=1000000000000,todo["ORE"])
+	mp=(lo+hi)//2
+	print("trying",mp,lo,hi)
+	if seek(mp):lo=mp
+	else:hi=mp
+		
