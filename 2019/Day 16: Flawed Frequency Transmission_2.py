a8a81a0d3ef18fc6370ad975e685a910833c70db
Ugadal <dcoorna@gmail.com>
2025-01-29 11:30:20 +0100

    up

--- 2019/Day 16: Flawed Frequency Transmission_2.py
+++ 2019/Day 16: Flawed Frequency Transmission_2.py
@@ -1,27 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 fn,part="d16.txt",0
-sep="\n\n"
-import math
-
-def gen(r):
-	V=[]
-	for c in [0,1,0,-1]:V.extend([c]*(r+1))
-	V.append(V.pop(0))
-	# ~ def ig(r):
-		# ~ I=[0,1,0,-1]
-		# ~ while True:
-			# ~ for c in I:
-				# ~ for x in range(r):
-					# ~ yield c
-	# ~ mg=ig(r+1)
-	# ~ next(mg)
-	while True:
-		for v in V:yield v
-		# ~ yield next(mg)
-def npnl(L):
-	# ~ print("".join(map(str,map(abs,L))))
-	return "".join(map(str,L))
+def npnl(L): return "".join(map(str,L))
 def ld(x):	return int(str(x)[-1])
 inp="03036732577212944063491565474664"
 inp="02935109699940807407585447034323"
@@ -35,8 +15,7 @@ p=z[pos:]
 print(len(p))
 for t in range(100):
 	np=[p.pop()]
-	for i in reversed(p):
-		np.append(ld(np[-1]+i))
+	for i in reversed(p):np.append(ld(np[-1]+i))
 	np.reverse()
 	p=np
 	print(npnl(p[:8]))
