9cccf855d4b668c0307b2b96150c1ae7d98b3908
Ugadal <dcoorna@gmail.com>
2025-02-05 16:33:28 +0100

    up

--- 2019/Day 18: Many-Worlds Interpretation.py
+++ 2019/Day 18: Many-Worlds Interpretation.py
@@ -40,56 +40,12 @@ def around(p):
 	yield p+1
 	yield p+1j
 	yield p-1j
-def gets(p):return [M.get(tp,"#") for tp in around(p)]
+def gets(p):
+	return [M.get(tp,"#") for tp in around(p)]
 
 def removedeadends(M):
 	DE=[k for k,v in M.items() if (v=="." or v in locks) and gets(k).count("#")>=3]
 	return DE
-while True:
-	TR=removedeadends(M)
-	if not TR:break
-	# ~ print(len(TR))
-	for k in TR:M[k]="#"
-draw()
-# ~ exit()
-# ~ input()
-todo=[(sp,"",0)]
-visited={}
-leave=False
-doit="n"
-# ~ doit=input("do partone ?")
-if doit=="y":doit=True
-else:doit=False
-while doit:
-	newtodo=[]
-	if leave:break
-	for pos,ks,steps in todo:
-		# ~ print(pos,ks,steps)
-		if leave:break
-		if all(s in ks for s in rk):
-			print(pos,steps-1,ks)
-			leave=True
-			break
-		if visited.get((pos,ks),float("Inf"))<steps:
-			continue
-		# ~ input()
-		cs=M.get(pos,"#")
-		if cs in keys and cs not in ks:
-			ks+=cs
-			ks=list(ks)
-			ks.sort()
-			ks="".join(ks)
-		if cs in locks and cs.lower() not in ks:continue
-		for np in around(pos):
-			if M.get(np,"#")=="#":continue
-			newtodo.append((np,ks,steps+1))
-		visited[pos,ks]=steps
-	todo=set(newtodo)
-	print(len(newtodo),len(todo),pos,ks,steps)
-	if not todo:break
-# ~ print (visited)
-# ~ p2
-# ~ exit()
 M[sp-1j]="#"
 M[sp+1j]="#"
 M[sp-1]="#"
@@ -97,10 +53,16 @@ M[sp+1]="#"
 M[sp]="#"
 SPS=(sp-1-1j,sp-1+1j,sp+1-1j,sp+1+1j)
 for s in SPS:M[s]="@"
+while True:
+	TR=removedeadends(M)
+	if not TR:break
+	# ~ print(len(TR))
+	for k in TR:M[k]="#"
 draw()
-DE=[(k,v) for k,v in M.items() if v!="#" and gets(k).count("#")==1]
-for k,v in DE:M[k]="§"
-draw()
+input()
+DE=[(k,v) for k,v in M.items() if v=="." and gets(k).count("#")==1 and v!="@"]
+# ~ for i,(k,v) in enumerate(DE):M[k]=f"fork{i}"
+# ~ draw()
 # ~ print(DE)
 NODES={}
 class nodes():
@@ -112,66 +74,88 @@ class nodes():
 		# ~ NODES[sym]=self
 		NODES[pos]=self
 		self.d=0
-		
+for k,v in M.items():
+	if v in locks or	v in keys:nodes(v,k)
+for i,(k,v) in enumerate(DE):
+	nodes(f"fork{i}",k)
+for i,k in enumerate(SPS):
+	nodes(f"start{i}",k)
+print(len(NODES))
+
+draw()
 while True:
-	try:pos,sy=next((k,v) for k,v in M.items() if v not in "#.@" and gets(k).count("#")>=3)
+	try:cn=next(node for node in NODES.values() if gets(node.pos).count("#")==3 and M[node.pos]!="#" and not node.sym.startswith("start"))
 	except:break
-	print(pos,sy)	
-	if pos in NODES:cn=NODES[pos]
-	else:cn=nodes(sy,pos)
-	print(cn)
-	pn=next(k for k in around(pos) if M.get(k,"#")!="#")
-	print(pn,M[pn])
-	if M[pn]==".":
-		M[pn]=sy
-		M[pos]="#"
-		cn.pos=pn
+	nxpos=next(k for k in around(cn.pos) if M.get(k,"#")!="#")
+	try:
+		v=next(node for node in NODES.values() if node.pos==nxpos)
+		print(v.sym)
+		cn.con[v]=cn.d+1
+		v.con[cn]=cn.d+1
+		M[cn.pos]="#"
+	except:
+		M[cn.pos]="#"
+		cn.pos=nxpos
 		cn.d+=1
-		# ~ draw()
-		# ~ input()
-		continue
-	if pn in NODES:nxn=NODES[pn]
-	else:nxn=nodes(sy,pos)
-	nxn.con[cn]=cn.d
-	cn.con[nxn]=cn.d
-	print(nxn.pos,nxn.con)
-	M[cn.pos]="#"
 	# ~ draw()
 	# ~ input()
-draw()
-for x in SPS:
-	cs=NODES[x]
-	print(cs.con)
-exit()
-def gets(L):return [M.get(p,"#") for p in L]
-todo=[(SPS,"",0)]
+# ~ for p in SPS:
+	# ~ s=NODES[p]
+	# ~ print(s.sym,[(t.sym,d) for t,d in s.con.items()])
+for node in NODES.values():
+	print (node.sym,[(p.sym,d) for p,d in node.con.items()])
+input()
+leave=False
+todo=[(tuple(NODES[p] for p in SPS),"",0)]
 visited={}
-print("=== P2 ===")
-while True:
+record=float("Inf")
+while todo:
 	newtodo=[]
-	for PS,ks,steps in todo:
-		if all(s in ks for s in rk):
-			print(*PS,steps-1,ks)
-			exit()
-		if visited.get((str(PS),ks),float("Inf"))<steps:continue
-		CS=gets(PS)
-		if any([cs in locks and cs.lower() not in ks for cs in CS]):continue
-		if any([cs in keys and cs not in ks for cs in CS]):
-			for cs in [cs for cs in CS if cs in keys and cs not in ks]:
-				ks+=cs
+	if leave:break
+	for cpos,ks,steps in todo:
+		# ~ print([p.sym for p in cpos],ks,steps)
+		# ~ input()
+		if leave:break
+		currsit="".join([p.sym for p in cpos])+ks
+		if visited.get(currsit,record)<steps:
+			# ~ print("skipping")
+			continue
+		# ~ print(currsit)
+		currkeys=[p.sym for p in cpos if p.sym in keys]
+		# ~ print(currkeys)
+		for k in currkeys:
+			if k not in ks:
+				ks+=k
 				ks=list(ks)
 				ks.sort()
 				ks="".join(ks)
-		for p,pos in enumerate(PS):
-			for np in around(pos):
-				if M.get(np,"#")=="#":continue
-				NP=list(PS)
-				NP[p]=np
-				newtodo.append((tuple(NP),ks,steps+1))
-				# ~ print(newtodo)
-				# ~ input()
-		visited[str(PS),ks]=steps
-	# ~ print(newtodo)
+		currlocks=[p.sym for p in cpos if p.sym in locks]
+		# ~ print(currlocks)
+		for l in currlocks:
+			if l not in ks:continue
+		if all(s in ks for s in rk):
+			print("ok:",pos,steps,ks)
+			record=steps
+			continue
+			leave=True
+			break
+		for n,d in cpos[0].con.items():
+			if n.sym in locks and n.sym.lower() not in ks:continue
+			newtodo.append(((n,cpos[1],cpos[2],cpos[3]),ks,steps+d))
+		for n,d in cpos[1].con.items():
+			if n.sym in locks and n.sym.lower() not in ks:continue
+			newtodo.append(((cpos[0],n,cpos[2],cpos[3]),ks,steps+d))
+		for n,d in cpos[2].con.items():
+			if n.sym in locks and n.sym.lower() not in ks:continue
+			newtodo.append(((cpos[0],cpos[1],n,cpos[3]),ks,steps+d))
+		for n,d in cpos[3].con.items():
+			if n.sym in locks and n.sym.lower() not in ks:continue
+			newtodo.append(((cpos[0],cpos[1],cpos[2],n),ks,steps+d))
+		visited[currsit]=steps
 	todo=set(newtodo)
-	print(len(todo),len(visited),PS,ks,steps)
-		
+	print(len(newtodo),len(todo),[p.sym for p in cpos],ks,steps)
+	if not todo:break
+# ~ < 2219
+# ~ <2162
+# ~ <2220 obviously
+# ~ ?>2187
