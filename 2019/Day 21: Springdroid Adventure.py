7be1389e36e4a0da535c22d214e4845d46aee120
Ugadal <dcoorna@gmail.com>
2025-02-11 07:24:33 +0100

    up

--- 2019/Day 21: Springdroid Adventure.py
+++ 2019/Day 21: Springdroid Adventure.py
@@ -3,80 +3,31 @@
 fn,part="d21.txt",0
 from functools import cache
 from intcodegen import computer
+import itertools as it
 pgm=open(fn).readline().strip()
-# ~ pgm="2"+pgm[1:]
-c=computer(pgm)
-# ~ for v in c.flow:print(chr(v),end="")
-c.inp.extend([*map(ord,"NOT A J\n")])
-c.inp.extend([*map(ord,"NOT B T\n")])
-c.inp.extend([*map(ord,"AND T J\n")])
-c.inp.extend([*map(ord,"NOT C T\n")])
-c.inp.extend([*map(ord,"AND T J\n")])
-c.inp.extend([*map(ord,"AND D J\n")])
-c.inp.extend([*map(ord,"WALK\n")])
-for v in c.flow:print(chr(v),end="")
-exit()
-M={}
-for row in range(47):
-	for col in range(150):
-		p=complex(col,row)
+pc=list(it.product(("AND","OR","NOT"),("A","B","C","D","T","J"),("T\n","J\n")))
+# ~ for p in pc:print(" ".join(p))
+pc=[" ".join(p) for p in pc]
+# ~ pc.remove("NOT D J\n")
+for r in range(4,16):
+	print("\n",r)
+	for x,p in enumerate(it.product(pc,repeat=r)):
+		print(x,end="\r")
+		if x<566200:continue
 		c=computer(pgm)
-		c.inp.extend([row,col])
-		for v in c.flow:
-			M[p]="#" if v else "."
-@cache
-def check(r,c):
-	C=computer(pgm)
-	C.inp.extend([r,c])
-	return next(C.flow)
-@cache
-def getlc(row):
-	if row<=5:return 8
-	b=getlc(row-1)
-	while not check(row,b):b+=1
-	return b
-@cache
-def getrc(row):
-	if row<=5:return 8
-	b=getrc(row-1)+5
-	while not check(row,b):b-=1
-	return b
+		c.inp.extend([*map(ord,"".join(p))])
+		c.inp.extend([*map(ord,"WALK\n")])
+		res=list(c.flow)
+		if res[34:40]!=list(map(ord,"Didn't")):
+			print (p)
+			print(res)
+			for v in res:
+				try:print(chr(v),end="")
+				except:continue
+			input()
+exit()
 
-def draw(M):
-	mic=int(min(m.real for m in M))
-	mxc=int(max(m.real for m in M))
-	mir=int(min(m.imag for m in M))
-	mxr=int(max(m.imag for m in M))
-	for r in range(mir,mxr+1):
-		R=[]
-		for c in range(mic,mxc+1):
-			pos=complex(c,r)
-			sym=M.get(pos,"?")
-			# ~ if pos==S:sym="S"
-			# ~ elif pos==cp:sym="D"
-			# ~ else:sym=M.get(pos,"?")
-			R.append(sym)
-		print("".join(R),r,getlc(r),getrc(r))
-	print() 
-draw(M)
-print(list(M.values()).count("#"))
-# ~ r=105
-d=100
-r=d+5
-while True:
-	r+=1
-	bl=getlc(r)
-	tr=getrc(r-d+1)
-	print(r,bl,tr)
-	if tr-bl>=d-1:
-		print(bl*10000+r-d+1)
-		print((r-d+1)*10000+bl)
-		break
-# ~ < 13370986
-# ~ < 13280979
-# ~ M={}
-# ~ for row in range(r-d,r+2):
-	# ~ for col in range(bl-1,tr+2):
-		# ~ p=complex(col,row)
-		# ~ M[p]="#" if check(row,col) else "."
-# ~ draw(M)
+# ~ 'OR A T\n'
+# ~ 'AND C T\n'
+# ~ 'NOT T J\n'
+# ~ 'AND D J\n'
