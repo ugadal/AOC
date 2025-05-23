78af883eb7dedab84b287a36c279f67fbd830fbf
Ugadal <dcoorna@gmail.com>
2025-05-23 15:04:50 +0200

    up

---
+++ 2020/Day 7: Handy Haversacks.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+fn,part="d7.txt",1
+data=open(fn).read().split("\n\n")[part].splitlines()
+import re
+r=re.compile(r"^(.*) bags contain (.*)\.$")
+sub=re.compile(r"(\d+) (.*) bags?")
+Bags={}
+# ~ p1
+for line in data:
+	# ~ print(line)
+	cat,cont=r.findall(line)[0]
+	# ~ print(cat,":",cont)
+	k=Bags[cat]=set()
+	for p in cont.split(", "):
+		if sub.match(p):
+			amount,nat=sub.findall(p)[0]
+			# ~ print(amount,":",nat)
+			k.add(nat)
+ab=set(Bags.keys())
+OK=set(k for k,v in Bags.items() if "shiny gold" in v)
+lastok=set()
+while lastok!=OK:
+	lastok=set(OK)
+	# ~ print(len(OK),OK)
+	OK=OK|set(k for k,v in Bags.items() if v&OK)
+print(len(OK))
