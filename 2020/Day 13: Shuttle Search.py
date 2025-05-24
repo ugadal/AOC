63002c787361da5b66ddb865b2961aeb902d980e
Ugadal <dcoorna@gmail.com>
2025-05-24 15:54:57 +0200

    up

---
+++ 2020/Day 13: Shuttle Search.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import re
+import sys
+cp=str(sys.argv[0])
+dre=re.compile(r"^Day (\d+):")
+day=dre.findall(cp)[0]
+fn,part=f"d{day}.txt",0
+data=open(fn).read().split("\n\n")[part].splitlines()
+pos=complex(0,0)
