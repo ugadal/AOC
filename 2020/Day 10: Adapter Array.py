93e6f4a5846ea8682662956585a66d9a54da47ea
David Coornaert <dcoorna@gmail.com>
2025-05-24 10:32:12 +0200

    up

--- 2020/Day 10: Adapter Array.py
+++ 2020/Day 10: Adapter Array.py
@@ -7,13 +7,8 @@ dre=re.compile(r"^Day (\d+):")
 day=dre.findall(cp)[0]
 fn,part=f"d{day}.txt",1
 data=list(map(int,open(fn).read().split("\n\n")[part].splitlines()))
-print(data)
 data.sort()
 data.insert(0,0)
 data.append(data[-1]+3)
-print(data)
 delta=[b-a for a,b in zip(data,data[1:])]
-print(delta)
-print(delta.count(1))
-print(delta.count(3))
-print(delta.count(1)*delta.count(3))
+print("p1:",delta.count(1)*delta.count(3))
