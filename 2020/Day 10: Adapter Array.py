ceae8d9ae0830dc5f3084f02debb28a07c5aa055
David Coornaert <dcoorna@gmail.com>
2025-05-24 10:31:37 +0200

    up

---
+++ 2020/Day 10: Adapter Array.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import re
+import sys
+cp=str(sys.argv[0])
+dre=re.compile(r"^Day (\d+):")
+day=dre.findall(cp)[0]
+fn,part=f"d{day}.txt",1
+data=list(map(int,open(fn).read().split("\n\n")[part].splitlines()))
+print(data)
+data.sort()
+data.insert(0,0)
+data.append(data[-1]+3)
+print(data)
+delta=[b-a for a,b in zip(data,data[1:])]
+print(delta)
+print(delta.count(1))
+print(delta.count(3))
+print(delta.count(1)*delta.count(3))
