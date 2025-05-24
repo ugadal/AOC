7c0cc8012c4ad4946fef202de6e3314f49c8a32f
Ugadal <dcoorna@gmail.com>
2025-05-24 14:13:02 +0200

    up

--- 2020/Day 12: Rain Risk.py
+++ 2020/Day 12: Rain Risk.py
@@ -5,5 +5,24 @@ import sys
 cp=str(sys.argv[0])
 dre=re.compile(r"^Day (\d+):")
 day=dre.findall(cp)[0]
-fn,part=f"d{day}.txt",0
+fn,part=f"d{day}.txt",1
 data=open(fn).read().split("\n\n")[part].splitlines()
+pos=complex(0,0)
+ext=re.compile(r"^(\S)(\d+)$")
+def rot(cd,rl,ang):
+	j=1j if rl=="R" else -1j
+	for _ in range(ang//90):cd*=j
+	return cd
+d=complex(1,0)
+for line in data:
+	i,n=ext.findall(line)[0]
+	n=int(n)
+	match i:
+		case "N":pos+=complex(0,-n)
+		case "S":pos+=complex(0,n)
+		case "E":pos+=complex(n,0)
+		case "W":pos+=complex(-n,0)
+		case "F":pos+=d*n
+		case "L":d=rot(d,i,n)
+		case "R":d=rot(d,i,n)
+print("p1:",pos.real+pos.imag)
