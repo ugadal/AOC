ec250eab2681720708122b66508a35d313ce81eb
Ugadal <dcoorna@gmail.com>
2025-07-03 09:58:37 +0200

    up

--- 2020/Day 19: Monster Messages.py
+++ 2020/Day 19: Monster Messages.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-part=0
+part=1
 import re
 import sys
 import itertools as it
@@ -21,12 +21,38 @@ for line in rules.splitlines():
 @cache
 def decompose(x):
 	print('entering on',x)
-	if x.startswith('"'):return [x[1:-1]]
+	if x.startswith('"'):
+		print("returning simple from",x,[x[1:-1]])
+		return [x[1:-1]]
 	if x.count(" | "):
-		return list(it.product(*map(decompose,x.split(" | "))))
+		P=[]
+		for p in map(decompose,x.split(" | ")):
+			P.extend(p)
+		print(f"returning from or {x} {P}")
+		return P
 	if x.count(" "):
-		return "".join(*it.product(*map(decompose,x.split(" "))))
-	if R[x].startswith('"'):return [R[x][1:-1]]
+		P=map(decompose,x.split(" "))
+		P=list(P)
+		# ~ print(P)
+		P=it.product(*P)
+		# ~ P=list(P)
+		# ~ print(P)
+		P=["".join(p) for p in P]
+		# ~ print(P)
+		print(f"returning from and {x} : {P}")
+		return P
+	# ~ if R[x].startswith('"'):return [R[x][1:-1]]
 	return decompose(R[x])
-print(decompose("4 | 5"))
-print(decompose("4 5 | 5 4 | 4 4 4"))
+# ~ 3: 4 5 | 5 4
+# ~ 4: "a"
+# ~ 5: "b"
+# ~ print(decompose("4 5 4"))
+# ~ print(decompose("4 | 5"))
+ok=decompose("0")
+nok=0
+for line in data.splitlines():
+	if line in ok:
+		print(line,"ok")
+		nok+=1
+	else:print(line,"wrong")
+print(f"p1: {nok}")
