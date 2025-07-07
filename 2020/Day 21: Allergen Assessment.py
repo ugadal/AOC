6abba5461d06b4a7ad20ab2e269f08582f722f97
Ugadal <dcoorna@gmail.com>
2025-07-07 11:36:04 +0200

    up

---
+++ 2020/Day 21: Allergen Assessment.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+part=0
+import re
+import sys
+import itertools as it
+from functools import cache
+import uuid
+cp=str(sys.argv[0])
+dre=re.compile(r"^Day (\d+):")
+day=dre.findall(cp)[0]
+fn=f"d{day}.txt"
+blocks=open(fn).read().split("\n\n")[part]
