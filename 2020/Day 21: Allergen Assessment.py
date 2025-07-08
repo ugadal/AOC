9a5962187e5adde38cd309510d5d7929c6d88045
Ugadal <dcoorna@gmail.com>
2025-07-08 09:13:52 +0200

    up

--- 2020/Day 21: Allergen Assessment.py
+++ 2020/Day 21: Allergen Assessment.py
@@ -13,15 +13,32 @@ fn=f"d{day}.txt"
 blocks=open(fn).read().split("\n\n")[part]
 IL=set()
 AL=set()
+R=[]
+IL=[]
+AL={}
 for line in blocks.splitlines():
-	il,al=line.split(" (")
+	il,al=line.split(" (contains ")
 	il=il.split()
-	al=al[:-1].split("contains ")[1]
-	al=al.split(", ")
-	IL|=set(il)
-	AL|=set(al)
+	al=al[:-1].split(", ")
+	IL+=il
+	for a in al:
+		if a in AL:AL[a]&=set(il)
+		else:AL[a]=set(il)
 print(IL)
-print(AL)
-for i,c in enumerate(it.permutations(IL,len(AL))):
-	pass
-print(i)
+for k,v in AL.items():print(k,v)
+# ~ af=s
+AF = set([i for v in AL.values() for i in v])
+SF = [i for i in IL if i not in AF]
+print(f'p1: {len(SF)} ')
+
+fixed={}
+while AL:
+	K=[(k,list(v)[0]) for k,v in AL.items() if len(v)==1]
+	print(K)
+	for k,v in K:
+		fixed[k]=v
+		del AL[k]
+		for a,b in AL.items():
+			if v in b:b.remove(v)
+for k,v in fixed.items():print(f"{k} -> {v}")
+print("p2:",",".join([v for k,v in sorted(fixed.items())]))
