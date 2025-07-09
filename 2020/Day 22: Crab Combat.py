b8b850e5c26e23f578b1d1a190cbd5088bd5815b
Ugadal <dcoorna@gmail.com>
2025-07-09 14:28:25 +0200

    up

--- 2020/Day 22: Crab Combat.py
+++ 2020/Day 22: Crab Combat.py
@@ -14,7 +14,7 @@ blocks=open(fn).read().split("\n\n\n")[part]
 pa,pb=blocks.split("\n\n")
 pa=list(map(int,(line for line in pa.splitlines()[1:])))
 pb=list(map(int,(line for line in pb.splitlines()[1:])))
-
+sys.setrecursionlimit(1000000000)
 while pa and pb:
 	a=pa.pop(0)
 	b=pb.pop(0)
@@ -27,12 +27,56 @@ while pa and pb:
 w=pa if pa else pb
 w.reverse()
 print("p1:",sum((i+1)*v for i,v in enumerate(w)))
-pa=list(map(int,(line for line in pa.splitlines()[1:])))
-pb=list(map(int,(line for line in pb.splitlines()[1:])))
+pa,pb=blocks.split("\n\n")
+pa=list(pa.splitlines()[1:])
+pb=list(pb.splitlines()[1:])
+# ~ @cache
+K={}
+# ~ @cache
 def game(A,B):
-	if not A:return "b",B
-	if not B:return "a",A
-	if A[0]>len[A] and B[0]>len(B):
+	# ~ global K
+	print(len(K))
+	oa=A
+	ob=B
+	print(oa)
+	print(ob)
+	if A+":"+B in K:return K[A+":"+B]
+	if B+":"+A in K:return "a" if K[A+":"+B]=="b" else "b"
+	if not A:
+		K[A+":"+B]="b"
+		print("p2:",sum((i+1)*int(v) for i,v in enumerate(reversed(B.split(",")))))
+		return "b"
+	if not B:
+		K[A+":"+B]="a"
+		print("p2:",sum((i+1)*int(v) for i,v in enumerate(reversed(A.split(",")))))
+		return "a"
+	# ~ print()
+	# ~ print("A:",A)
+	# ~ print("B:",B)
+	A=list(A.split(","))
+	B=list(B.split(","))
+	# ~ print(f"A:{A}")
+	# ~ print(f"B:{B}")
+	# ~ print("p2:",sum((i+1)*int(v) for i,v in enumerate(reversed(A))))
+	# ~ print("p2:",sum((i+1)*int(v) for i,v in enumerate(reversed(B))))
+	# ~ print("p2:",sum((i+1)*int(v) for i,v in enumerate(reversed(A))))
+	# ~ print("p2:",sum((i+1)*int(v) for i,v in enumerate(reversed(B))))
+	if int(A[0])<len(A) and int(B[0])<len(B) and True:
 		hold=A[0],B[0]
-		winner,deck=game(A[1:],B[1:])
-		
+		winner=game(",".join(A[1:1+int(A[0])]),",".join(B[1:1+int(B[0])]))
+		if winner=="a":
+			res=game(",".join(A[1:]+[hold[0],hold[1]]),",".join(B[1:]))
+		else:
+			res=game(",".join(A[1:]),",".join(B[1:]+[hold[1],hold[0]]))
+		K[oa+":"+ob]=res
+		return res
+	if int(A[0]) > int(B[0]):
+		res=game(",".join(A[1:]+[A[0],B[0]]),",".join(B[1:]))
+	else:
+		res=game(",".join(A[1:]),",".join(B[1:]+[B[0],A[0]]))
+	K[oa+":"+ob]=res
+	return res
+# ~ print(game([10],[8,9]))	
+# ~ print(pa)
+print(game(",".join(pa),",".join(pb)))
+
