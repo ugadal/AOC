4f860d8efbaa35701a2a867e32b3d187d6afbf67
David Coornaert <dcoorna@gmail.com>
2025-07-11 09:03:13 +0200

    up

--- 2020/Day 24: Lobby Layout.py
+++ 2020/Day 24: Lobby Layout.py
@@ -37,13 +37,52 @@ def gp(s):
 			c-=1
 			s=s[2:]
 	return r,c
-print(gp("esew"))
-print(gp("nwwswee"))
 F=[gp(s) for s in blocks.splitlines()]
-print(F,len(F))
 B={}
 for p in F:
 	if p in B:
 		B.pop(p)
 	else:B[p]=True
+print("p1:",len(B))
+B={}
+for p in F:
+	if p in B:B[p]=not(B[p])
+	else:B[p]=True
+def nb(r,c):
+	yield r+2,c
+	yield r-2,c
+	yield r+1,c+1
+	yield r-1,c+1
+	yield r+1,c-1
+	yield r-1,c-1
+def gnb(r,c):
+	return [B.get(p,False) for p in nb(r,c)]
 print(B,len(B))
+for x in range(1,101):
+	TF=[]
+	Bl=[b for b,v in B.items() if v]
+	Wh=[b for b,v in B.items() if not v]
+	# ~ print(Bl,len(Bl))
+	# ~ print(Wh,len(Wh))
+	for b in Bl:
+		ba=gnb(*b).count(True)
+		if ba==0 or ba>2:TF.append(b)
+	# ~ print("black to flip",TF)
+	mir=min(r for r,c in Bl)
+	mar=max(r for r,c in Bl)
+	mic=min(c for r,c in Bl)
+	mac=max(c for r,c in Bl)
+	# ~ print(mir,mic,mar,mac)
+	for r in range(mir-2,mar+3):
+		for c in range(mic-2,mac+3):
+			if (r+c)%2:continue
+			if B.get((r,c),False):continue
+			N=gnb(r,c)
+			# ~ print(r,c,N)
+			if N.count(True)==2:TF.append((r,c))
+	# ~ print("tf",TF)
+	for p in TF:
+		B[p]=not B.get(p,False)
+	Bl=[b for b,v in B.items() if v]
+	print(x,":",len(Bl))
+	
