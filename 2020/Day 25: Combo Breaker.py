76043e1b479bf3354670e94a9e9ffc1559feb44a
David Coornaert <dcoorna@gmail.com>
2025-07-11 14:53:53 +0200

    up

--- 2020/Day 25: Combo Breaker.py
+++ 2020/Day 25: Combo Breaker.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-part=1
+part=0
 import re
 import sys
 import itertools as it
@@ -11,3 +11,23 @@ dre=re.compile(r"^Day (\d+):")
 day=dre.findall(cp)[0]
 fn=f"d{day}.txt"
 blocks=open(fn).read().split("\n\n")[part]
+def tr(i,l):
+	return (i%20201227)**l%20201227
+cp=tr(7,8)
+dp=tr(7,11)
+print(tr(dp,8))
+print(tr(cp,11))
+# ~ 18356117
+i=1
+c=0
+while True:
+	i=i*7%20201227
+	c+=1
+	if i==5909654:break
+
+# ~ 18356117>3974372
+# ~ 5909654>8623737
+
+
+print(c)
+print(tr(5909654,3974372))
