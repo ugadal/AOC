47871dca96b57aeed8719ebc42b44f210698eefe
David Coornaert <dcoorna@gmail.com>
2025-07-12 21:34:48 +0200

    up

--- 2021/Day 9: Smoke Basin.py
+++ 2021/Day 9: Smoke Basin.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 part=1
+
 import re
 import sys
 import itertools as it
@@ -24,7 +25,34 @@ def around(pos):
 		np=pos+offs
 		if np in G:yield G[np]
 res=0
+lp=[]
 for k,v in G.items():
 	if all(x>v for x in around(k)):
 		res+=v+1
+		lp.append(k)
 print("p1:",res)
+def around(pos):
+	for offs in (-1,1,1j,-1j):
+		np=pos+offs
+		if np in G:yield np
+
+print(lp)
+R=[]
+for p in lp:
+	todo=[p]
+	done=[]
+	while todo:
+		pos=todo.pop(0)
+		if pos in done:continue
+		done.append(pos)
+		ch=G[pos]
+		for n in around(pos):
+			if G[n]==9:continue
+			if G[n]>ch:todo.append(n)
+	print(len(done))
+	R.append(done)
+R=sorted(R,key=lambda s:len(s),reverse=True)
+R=R[:3]
+res=1
+for r in R:res*=len(r)
+print(res)
