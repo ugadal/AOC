c5ab73f32a357aee6177528a6e90df9b7f0548d5
David Coornaert <dcoorna@gmail.com>
2025-07-12 11:01:59 +0200

    up

--- 2021/Day 6: Lanternfish.py
+++ 2021/Day 6: Lanternfish.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-part=0
+part=1
 import re
 import sys
 import itertools as it
@@ -12,3 +12,11 @@ day=dre.findall(cp)[0]
 fn=f"d{day}.txt"
 # ~ block=open(fn).read().split("\n\n\n")[part]
 block=open(fn).read().split("\n\n")[part]
+BT={v:0 for v in range(9)}
+V=tuple(map(int,block.split(",")))
+V=[V.count(x) for x in range(9)]
+for x in range(80):
+	z=V.pop(0)
+	V=V+[z]
+	V[6]+=z
+print("p1:",sum(V))
