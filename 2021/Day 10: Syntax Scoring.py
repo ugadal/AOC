b93fdd34bd84d441da0dc0e33a30e9064fb3b0d3
David Coornaert <dcoorna@gmail.com>
2025-07-12 21:57:21 +0200

    up

---
+++ 2021/Day 10: Syntax Scoring.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+part=1
+
+import re
+import sys
+import itertools as it
+from functools import cache
+import uuid
+cp=str(sys.argv[0])
+dre=re.compile(r"^Day (\d+):")
+day=dre.findall(cp)[0]
+fn=f"d{day}.txt"
+# ~ block=open(fn).read().split("\n\n\n")[part]
+block=open(fn).read().split("\n\n")[part]
+# ~ [({(<(())[]>[[{[]{<()<>>
+# ~ [({([[{{
+# ~ {([(<{}[<>[]}>{[]{[(<()> incomplete
+# ~ {([(<[}>{{[( invalid
+SC={")": 3 ,
+"]": 57,
+"}": 1197,
+">": 25137}
+res=0
+for s in block.splitlines():
+	# ~ print(s)
+	while True:
+		ori=s
+		s=s.replace("()","")
+		s=s.replace("[]","")
+		s=s.replace("<>","")
+		s=s.replace("{}","")
+		if s==ori:break
+	if all(s.count(x)==0 for x in ")]>}"):continue
+	print(s)
+	loc=min(s.index(x) for x in ")]>}" if s.count(x))
+	res+=SC[s[loc]]
+print("p1;",res)
