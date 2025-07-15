7153c93742533799557dec8bd83c79b6d26a06f6
Ugadal <dcoorna@gmail.com>
2025-07-15 19:33:56 +0200

    up

---
+++ 2021/Day 15: Chiton.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+part=0
+
+import re
+import sys
+import itertools as it
+from functools import cache
+import uuid
+cp=str(sys.argv[0])
+dre=re.compile(r"^Day (\d+):")
+day=dre.findall(cp)[0]
+fn=f"d{day}.txt"
+block=open(fn).read().split("\n\n\n")[part]
+# ~ block=open(fn).read().split("\n\n")[part]
