5a5bc0044c6994edb9b078fe607fa546acc67599
Ugadal <dcoorna@gmail.com>
2025-07-16 07:36:43 +0200

    up

--- 2021/Day 15: Chiton.py
+++ 2021/Day 15: Chiton.py
@@ -39,3 +39,59 @@ while todo:
 			todo.add(tar)
 ep=complex(C,R)
 print("p1:",BP[ep])
+
+G={}
+BP={}
+for r,row in enumerate(block.splitlines()):
+	for c,s in enumerate(row):
+		pos=complex(c,r)
+		G[pos]=int(s)
+		BP[pos]=float("Inf")
+
+pos=list(G.keys())
+C+=1
+R+=1
+
+for rb in range(1,5):
+	for p in pos:
+		np=p+complex(rb*C,0)
+		v=G[p]+rb
+		v=v%9 if v>9 else v
+		G[np]=v
+
+pos=list(G.keys())
+for db in range(1,5):
+	for p in pos:
+		np=p+complex(0,db*R)
+		v=G[p]+db
+		v=v%9 if v>9 else v
+		G[np]=v
+
+ec=max(p.real for p in G.keys())
+er=max(p.imag for p in G.keys())
+ep=complex(ec,er)
+print(ep)
+for p in G.keys():BP[p]=float("Inf")
+BP[sp]=0
+todo=set([sp])
+while todo:
+	cp=todo.pop()
+	cw=BP[cp]
+	for tar in around(cp):
+		v=G[tar]
+		nv=cw+v
+		if nv<BP[tar]:
+			BP[tar]=nv
+			todo.add(tar)
+print("p2:",BP[ep])
+
+
+
+
+
+
+
+
+
+
+
