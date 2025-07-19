11a2a4280e9d16c938531958ac689e044cc9c04d
Ugadal <dcoorna@gmail.com>
2025-07-19 07:36:21 +0200

    up

--- 2021/Day 16: Packet Decoder.py
+++ 2021/Day 16: Packet Decoder.py
@@ -18,13 +18,22 @@ def getbs(line):
 	while len(r)%4:r="0"+r
 	# ~ print(r)
 	return r
-
+def operate(ptid,V):
+	match ptid:
+		case 0:v=sum(V)
+		case 1:
+			v=1
+			for t in V:v*=t	
+		case 2:v=min(V)
+		case 3:v=max(V)
+		case 5:v=1 if V[0]>V[1] else 0
+		case 6:v=1 if V[0]<V[1] else 0
+		case 7:v=1 if V[0]==V[1] else 0
+	return v
 def treat(r,lvl=1):
 	global ttv
 	decal=" ="*lvl
-	# ~ print(decal,r)
 	pv,r=int(r[:3],2),r[3:]
-	# ~ print(decal,"pv",pv)
 	ptid,r=int(r[:3],2),r[3:]
 	ttv+=pv
 	if ptid==4:
@@ -39,44 +48,22 @@ def treat(r,lvl=1):
 		ltid,r=r[0],r[1:]
 		if ltid=="1":
 			nsp,r=int(r[:11],2),r[11:]
-			# ~ print(decal,"nsp",nsp)
 			V=[]
 			for sp in range(nsp):
 				v,r=treat(r,lvl+1)
 				V.append(v)
-			match ptid:
-				case 0:v=sum(V)
-				case 1:
-					v=1
-					for t in V:v*=t	
-				case 2:v=min(V)
-				case 3:v=max(V)
-				case 5:v=1 if V[0]>V[1] else 0
-				case 6:v=1 if V[0]<V[1] else 0
-				case 7:v=1 if V[0]==V[1] else 0
+			v=operate(ptid,V)
 			return v,r
 		else:
 			ttlb,r=int(r[:15],2),r[15:]
-			# ~ print(decal,ttlb)
 			tex,r=r[:ttlb],r[ttlb:]
 			V=[]
 			while tex:
 				t,tex=treat(tex,lvl+1)
 				V.append(t)
-			match ptid:
-				case 0:v=sum(V)
-				case 1:
-					v=1
-					for t in V:v*=t	
-				case 2:v=min(V)
-				case 3:v=max(V)
-				case 5:v=1 if V[0]>V[1] else 0
-				case 6:v=1 if V[0]<V[1] else 0
-				case 7:v=1 if V[0]==V[1] else 0
+			v=operate(ptid,V)
 			return v,r
 ttv=0
 for line in open(fn).read().splitlines()[7:]:
-# ~ for line in open(fn).read().splitlines():
-	# ~ print(line)
 	print("p2:",treat(getbs(line)))
 print("p1:",ttv)
