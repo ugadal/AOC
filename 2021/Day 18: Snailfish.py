045f14d378292405f62f5dc3b967c4091d7195d3
Ugadal <dcoorna@gmail.com>
2025-07-20 10:17:19 +0200

    up

---
+++ 2021/Day 18: Snailfish.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+part=0
+
+import re
+import sys
+import itertools as it
+from functools import cache
+import uuid
+cp=str(sys.argv[0])
+dre=re.compile(r"^Day (\d+):")
+day=dre.findall(cp)[0]
+fn=f"d{day}.txt"
+# ~ block=open(fn).read().split("\n\n\n")[part]
+# ~ block=open(fn).read().split("\n\n")[part]
+# ~ target area: x=192..251, y=-89..-59
+lc,rc,br,tr=
