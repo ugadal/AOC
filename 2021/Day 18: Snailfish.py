065810bef0fb30b94ec47b5e6939f60747ce4379
Ugadal <dcoorna@gmail.com>
2025-07-21 09:17:36 +0200

    up

--- 2021/Day 18: Snailfish.py
+++ 2021/Day 18: Snailfish.py
@@ -20,6 +20,7 @@ class val():
 	def rep(self):return self.v
 	def vis(self):yield self
 	def visp(self):yield self
+	def mag(self):return self.v
 class pair():
 	ap=[]
 	def __init__(self):
@@ -54,6 +55,8 @@ class pair():
 		for v in self.a.visp():yield v
 		for v in self.b.visp():yield v
 		yield self
+	def mag(self):
+		return 3*self.a.mag()+2*self.b.mag()
 s="[1,2]"
 pre=re.compile(r"^\[.*\]$")
 def smartsplit(s):
@@ -76,64 +79,103 @@ def ana(s):
 		tp.b=ana(b)
 		return tp
 	else:return "?"
-l="[[[[[9,8],1],2],3],4]"
-l="[7,[6,[5,[4,[3,2]]]]]"
-l="[[6,[5,[4,[3,2]]]],1]"
-l="[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]"
-l="[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]"
-pair.ap=[]
-tree=ana(l)
+def simplify(tree):
+	while True:
+		ap=[p for p in tree.visp() if type(p)==pair]
+		# ~ print(len(ap))
+		while any(p.pp==4 for p in ap):
+			# ~ print("explodes")
+			pb=next(p for p in ap if p.pp==4)
+			# ~ print(pb.num)
+			lv,rv=pb.a,pb.b
+			# ~ for p in tree.vis():print(p,p.v)
+			ov=[p for p in tree.vis()]
+			if ov[0]==lv:pass
+			else:
+				for x,y in zip(ov,ov[1:]):
+					if y==lv:break
+				# ~ print(x.v,y.v)
+				x.v+=y.v
+			if ov[-1]==rv:pass
+			else:
+				for x,y in zip(ov,ov[1:]):
+					if x==rv:break
+				# ~ print(x.v,y.v)
+				y.v+=x.v
+			if pb.par.a==pb:pb.par.a=val(0)
+			else:pb.par.b=val(0)
+			# ~ pair.ap.remove(pb)
+			# ~ for p in tree.vis():print(p,p.v)
+			tree.mkdep()
+			ap=[p for p in tree.visp() if type(p)==pair]
+			# ~ print(tree.rep())
+		
+		split=False
+		if any(p.v>9 for p in tree.vis()):
+			split=True
+			# ~ print("splits")
+			pv=next(p for p in tree.vis() if p.v>9)
+			# ~ print(pv,pv.v)
+			# ~ input()
+			na=pv.v//2
+			nb=pv.v-na
+			np=pair()
+			np.a=val(na)
+			np.b=val(nb)
+			for pos in tree.visp():
+				if type(pos)==val:continue
+				if pos.a==pv:
+					pos.a=np
+					break
+				if pos.b==pv:
+					pos.b=np
+					break
+			tree.mkdep()
+		if split:continue
+		# ~ print(tree.rep())
+		return tree
+# ~ l="[[[[[9,8],1],2],3],4]"
+# ~ l="[7,[6,[5,[4,[3,2]]]]]"
+# ~ l="[[6,[5,[4,[3,2]]]],1]"
+# ~ l="[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]"
+# ~ l="[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]"
+# ~ pair.ap=[]
+# ~ tree=ana(l)
+# ~ tree.mkdep()
+# ~ res=simplify(tree)
+# ~ print(res.rep())
+lines="""[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
+[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
+[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
+[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]
+[7,[5,[[3,8],[1,4]]]]
+[[2,[2,2]],[8,[8,1]]]
+[2,9]
+[1,[[[9,3],9],[[9,0],[0,7]]]]
+[[[5,[7,4]],7],1]
+[[[[4,2],2],6],[8,7]]""".splitlines()
+lines="""[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+[[[5,[2,8]],4],[5,[[9,9],0]]]
+[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+[[[[5,4],[7,7]],8],[[8,3],8]]
+[[9,3],[[9,9],[6,[4,9]]]]
+[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]""".splitlines()
+lines=open(fn).read().splitlines()
+tree=ana(lines[0])
 tree.mkdep()
-# ~ print(l,[p.pp for p in pair.ap])
-while True:
-	while any(p.pp==4 for p in pair.ap):
-		print("explodes")
-		pb=next(p for p in pair.ap if p.pp==4)
-		print(pb.num)
-		lv,rv=pb.a,pb.b
-		# ~ for p in tree.vis():print(p,p.v)
-		ov=[p for p in tree.vis()]
-		if ov[0]==lv:pass
-		else:
-			for x,y in zip(ov,ov[1:]):
-				if y==lv:break
-			# ~ print(x.v,y.v)
-			x.v+=y.v
-		if ov[-1]==rv:pass
-		else:
-			for x,y in zip(ov,ov[1:]):
-				if x==rv:break
-			# ~ print(x.v,y.v)
-			y.v+=x.v
-		if pb.par.a==pb:pb.par.a=val(0)
-		else:pb.par.b=val(0)
-		pair.ap.remove(pb)
-		# ~ for p in tree.vis():print(p,p.v)
-		tree.mkdep()
-		print(tree.rep())
-	
-	split=False
-	if any(p.v>9 for p in tree.vis()):
-		split=True
-		print("splits")
-		pv=next(p for p in tree.vis() if p.v>9)
-		# ~ print(pv,pv.v)
-		# ~ input()
-		na=pv.v//2
-		nb=pv.v-na
-		np=pair()
-		np.a=val(na)
-		np.b=val(nb)
-		for pos in tree.visp():
-			if type(pos)==val:continue
-			if pos.a==pv:
-				pos.a=np
-				break
-			if pos.b==pv:
-				pos.b=np
-				break
-		tree.mkdep()
-	if split:continue
-	print(tree.rep())
-	break
 print(tree.rep())
+for l in lines[1:]:
+	nb=ana(l)
+	np=pair()
+	np.a=tree
+	np.b=nb
+	np.mkdep()
+	# ~ print(np.rep())
+	# ~ input()
+	tree=simplify(np)
+print(tree.rep())
+print("p1:",tree.mag())
