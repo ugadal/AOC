2a1fc0f3a2e9dd9db15fe14c960e12d3cdb191fd
Ugadal <dcoorna@gmail.com>
2025-07-24 20:02:47 +0200

    up

--- 2021/Day 22: Reactor Reboot.py
+++ 2021/Day 22: Reactor Reboot.py
@@ -6,8 +6,8 @@ import re
 import sys
 import itertools as it
 from functools import cache
-import uuid
-import numpy as np
+# ~ import uuid
+# ~ import numpy as np
 cp=str(sys.argv[0])
 dre=re.compile(r"^Day (\d+):")
 day=dre.findall(cp)[0]
@@ -62,32 +62,48 @@ lz=sorted(list(set(lz)))
 		# ~ for z,ez in zip(lz,lz[1:]):
 			# ~ cube(x,y,z,ex,ey,ez)
 # ~ print(len(cube.a))
-ON=set()
-for line in block.splitlines():
-	print("\r",line,end="")
+ON=[]
+mx=len(block.splitlines())
+UNIQ=[]
+for lix,line in enumerate(block.splitlines()):
+	print(lix,"/",mx,line)
+	ON=[]
 	st,coord=line.split()
 	st=True if st=="on" else False
+	if not st:continue
 	rx,ry,rz=coord.split(",")
 	V=[tuple(map(int,(r[2:].split("..")))) for r in coord.split(",")]
 	(bx,ex),(by,ey),(bz,ez)=V
-	if st:
-		for cx,cex in zip(lx,lx[1:]):
-			if bx<=cx<cex<=ex+1:
-				for cy,cey in zip(ly,ly[1:]):
-					if by<=cy<cey<=ey+1:
-						for cz,cez in zip(lz,lz[1:]):
-							if bz<=cz<cez<=ez+1:
-								ON.add((cx,cy,cz,cex,cey,cez))
-	else:
-		toremove=set()
+	# ~ if st:
+	for cx,cex in zip(lx,lx[1:]):
+		if bx<=cx<cex<=ex+1:
+			for cy,cey in zip(ly,ly[1:]):
+				if by<=cy<cey<=ey+1:
+					for cz,cez in zip(lz,lz[1:]):
+						if bz<=cz<cez<=ez+1:
+							ON.append((cx,cy,cz,cex,cey,cez))
+		ON=list(set(ON))
+	for offline in block.splitlines()[lix+1:]:
+		st,coord=offline.split()
+		st=True if st=="on" else False
+		if st:continue
+		V=[tuple(map(int,(r[2:].split("..")))) for r in coord.split(",")]
+		(bx,ex),(by,ey),(bz,ez)=V
+		toremove=[]
 		for cx,cy,cz,cex,cey,cez in ON:
 			if bx<=cx<cex<=ex+1:
 				if by<=cy<cey<=ey+1:
 					if bz<=cz<cez<=ez+1:
-						toremove.add((cx,cy,cz,cex,cey,cez))
-		ON=ON-toremove
+						toremove.append((cx,cy,cz,cex,cey,cez))
+		ON=set(ON)-set(toremove)
+	for uniq in UNIQ:
+		ON=ON-uniq
+	UNIQ.append(ON)
+		# ~ ON=[x for x in ON if not x in toremove]
+	print("\r-",len(ON),"-",end="")
 	# ~ break
 res=0
-for cx,cy,cz,cex,cey,cez in ON:
-	res+=(cex-cx)*(cey-cy)*(cez-cz)
+for ON in UNIQ:
+	for cx,cy,cz,cex,cey,cez in ON:
+		res+=(cex-cx)*(cey-cy)*(cez-cz)
 repres (2,res)
