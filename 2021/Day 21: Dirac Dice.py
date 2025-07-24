2950624de38977e46ee8a0b1dde5624eb551155b
Ugadal <dcoorna@gmail.com>
2025-07-24 08:28:47 +0200

    up

--- 2021/Day 21: Dirac Dice.py
+++ 2021/Day 21: Dirac Dice.py
@@ -12,69 +12,57 @@ cp=str(sys.argv[0])
 dre=re.compile(r"^Day (\d+):")
 day=dre.findall(cp)[0]
 fn=f"d{day}.txt"
-block=open(fn).read().split("\n\n\n")[part]
+# ~ block=open(fn).read().split("\n\n\n")[part]
 # ~ block=open(fn).read().split("\n\n")[part]
-pgm,img=block.split("\n\n")
-pgm="".join(pgm.splitlines())
-# ~ pgm="#"+pgm[1:]
-G={}
-for ir,row in enumerate(img.splitlines()):
-	for ic,s in enumerate(row):
-		pos=complex(ic,ir)
-		G[pos]=s
-def getedge():
-	mir=min(p.imag for p,v in G.items())
-	mar=max(p.imag for p,v in G.items())
-	mic=min(p.real for p,v in G.items())
-	mac=max(p.real for p,v in G.items())
-	return map(int,(mir,mar,mic,mac))
-def draw():
-	mir,mar,mic,mac=getedge()
-	for row in range(mir-2,mar+3):
-		R=[]
-		for col in range(mic-2,mac+3):
-			pos=complex(col,row)
-			R.append("#" if G.get(pos,defa)=="#" else ".")
-		print("".join(R))
-	print()
-	input()
-def around(pos):
-	yield pos-1-1j
-	yield pos-1j
-	yield pos+1-1j
-	yield pos-1
-	yield pos
-	yield pos+1
-	yield pos-1+1j
-	yield pos+1j
-	yield pos+1+1j
-mir,mar,mic,mac=getedge()
-defa="."
-for enhance in range(50):
-	print("\r",enhance,end="")
-	NG={}
-	for row in range(mir-1,mar+2):
-		for col in range(mic-1,mac+2):
-			pos=complex(col,row)
-			t="".join("1" if G.get(z,defa)=="#" else "0" for z in around(pos))
-			pp=int(t,2)
-			pp=pgm[pp]
-			NG[pos]=pp
-	G=NG
-	if enhance==1:
-		r=[]
-		for k,v in G.items():
-			if v==".":continue
-			r.append(k)
-		print("\np1:",len(r))		
-	defa="#" if defa=="." else "."
-	mir-=1
-	mar+=1
-	mic-=1
-	mac+=1
-r=[]
-for k,v in G.items():
-	if v==".":continue
-	r.append(k)
-print("\np2:",len(r))
-
+a,b,c=0,1,2
+pa,pb=4,8
+pa,pb=9,6
+sa=sb=0
+rolls=3
+pa-=1
+pb-=1
+while True:
+	pa=(pa+a+b+c+3)%10
+	sa+=pa+1
+	if sa>=1000:
+		print("a wins")
+		break
+	rolls+=3
+	a=(a+3)%100
+	b=(b+3)%100
+	c=(c+3)%100
+	pb=(pb+a+b+c+3)%10
+	sb+=pb+1
+	if sb>=1000:
+		print("b wins")
+		break
+	rolls+=3
+	a=(a+3)%100
+	b=(b+3)%100
+	c=(c+3)%100
+	# ~ exit()
+print(sa,sb,rolls)
+print("p1:",rolls*min(sa,sb))
+pa,pb=4,8
+pa,pb=9,6
+oc=[x + y + z for x in [1, 2, 3] for y in [1, 2, 3] for z in [1, 2, 3]]
+@cache
+def rec(pa,pb,sa,sb):
+	print(pa,pb,sa,sb)
+	# ~ input()
+	if sa>=21:return (1,0)
+	sc=[0,0]
+	for d in oc:
+		npa=pa+d
+		npa=(npa-1)%10+1
+		nsa=sa+npa
+		if nsa>=21:sc[0]+=1
+		else:
+			wb,wa=rec(pb,npa,sb,nsa)
+			sc[1]+=wb
+			sc[0]+=wa
+	return sc
+# ~ r=rec(4,8,0,0)
+# ~ print(r,max(r))
+r=rec(9,6,0,0)
+print(r,max(r))
