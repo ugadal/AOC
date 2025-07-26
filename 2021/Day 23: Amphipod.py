660770ac0e1a4c8b2bd7981ffac355a2d6d5f80c
Ugadal <dcoorna@gmail.com>
2025-07-26 15:08:19 +0200

    up

---
+++ 2021/Day 23: Amphipod.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+part=0
+def repres(p,v):print(f"p{p}: {v}")
+import re
+import sys
+import itertools as it
+from functools import cache
+# ~ import uuid
+# ~ import numpy as np
+cp=str(sys.argv[0])
+dre=re.compile(r"^Day (\d+):")
+day=dre.findall(cp)[0]
+fn=f"d{day}.txt"
+block=open(fn).read().split("\n\n\n")[part]
+# ~ block=open(fn).read().split("\n\n")[part]
+
