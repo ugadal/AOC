9c7cc484a61d56ef239fce5acaa77dd491f471fd
Ugadal <dcoorna@gmail.com>
2025-07-26 07:42:35 +0200

    up

--- 2021/Day 22: Reactor Reboot.py
+++ 2021/Day 22: Reactor Reboot.py
@@ -14,15 +14,16 @@ day=dre.findall(cp)[0]
 fn=f"d{day}.txt"
 block=open(fn).read().split("\n\n\n")[part]
 # ~ block=open(fn).read().split("\n\n")[part]
+
 G={}
 I=[]
 for line in block.splitlines():
 	st,coord=line.split()
 	st=True if st=="on" else False
-	# ~ rx,ry,rz=coord.split(",")
 	V=[tuple(map(int,(r[2:].split("..")))) for r in coord.split(",")]
 	(bx,ex),(by,ey),(bz,ez)=V
-	I.append((st,bx,ex,by,ey,bz,ez))
+	# ~ I.append((st,bx,ex,by,ey,bz,ez))
+	I.append((st,bx,by,bz,ex,ey,ez))
 	skip=any(b>50 or a<-50	for a,b in V)
 	# ~ print(V,skip)
 	if skip:continue
@@ -33,44 +34,99 @@ def ana(TI):
 	lx=[]
 	ly=[]
 	lz=[]
-	for st,bx,ex,by,ey,bz,ez in TI:
+	for st,bx,by,bz,ex,ey,ez in TI:
 		lx.append(bx)
-		lx.append(ex)
+		lx.append(ex+1)
 		ly.append(by)
-		ly.append(ey)
+		ly.append(ey+1)
 		lz.append(bz)
-		lz.append(ez)
+		lz.append(ez+1)
 	lx=sorted(list(set(lx)))
 	ly=sorted(list(set(ly)))
 	lz=sorted(list(set(lz)))
 	ON=[]
-	st,bx,ex,by,ey,bz,ez=TI[0]
+	st,bx,by,bz,ex,ey,ez=TI[0]
 	for cx,cex in zip(lx,lx[1:]):
 		if bx<=cx<cex<=ex+1:
 			for cy,cey in zip(ly,ly[1:]):
 				if by<=cy<cey<=ey+1:
 					for cz,cez in zip(lz,lz[1:]):
 						if bz<=cz<cez<=ez+1:
-							ON.append((cx,cy,cz,cex,cey,cez))
+							ON.append((cx,cy,cz,cex-1,cey-1,cez-1))
 	ON=set(ON)
-	for st,bx,ex,by,ey,bz,ez in TI[1:]:
+	for st,bx,by,bz,ex,ey,ez in TI[1:]:
 		toremove=[]
 		for cx,cy,cz,cex,cey,cez in ON:
 			if bx<=cx<cex<=ex+1:
 				if by<=cy<cey<=ey+1:
 					if bz<=cz<cez<=ez+1:
-						toremove.append((cx,cy,cz,cex,cey,cez))
+						toremove.append((cx,cy,cz,cex-1,cey-1,cez-1))
 		ON=ON-set(toremove)
 	return ON
+def isincluded(a,b):
+	abx,aby,abz,aex,aey,aez=a
+	bbx,bby,bbz,bex,bey,bez=b
+	if bbx<=abx and aex<=bex:
+		if bby<=aby and aey<=bey:
+			if bbz<=abz and aez<=bez:return True
+	return False
+U=set()
+print("collecting")
 for ix,quad in enumerate(I):
-	st,bx,ex,by,ey,bz,ez=quad
+	st,bx,by,bz,ex,ey,ez=quad
+	if not st:continue
+	print("\r",ix,quad,end="")
 	Z=[quad]
 	Z.extend(q for q in I[ix+1:] if not q[0])
 	res=ana(Z)
-	print(len(res))
-
-# ~ res=0
+	# ~ print("removing included from new")
+	torem=set()
+	for a,b in it.product(U,res):
+		if isincluded(a,b):torem.add(a)
+	U=U-torem
+	# ~ print("removing new from included")
+	torem=set()
+	for a,b in it.product(res,U):
+		if isincluded(a,b):torem.add(a)
+	res=res-torem
+	# ~ print(len(res))
+	U=U|res
+	# ~ print(len(U))
+print("collecting done")
+print(len(U))
+lx=[]
+ly=[]
+lz=[]
+for bx,by,bz,ex,ey,ez in U:
+	lx.append(bx)
+	lx.append(ex)
+	ly.append(by)
+	ly.append(ey)
+	lz.append(bz)
+	lz.append(ez)
+lx=sorted(list(set(lx)))
+ly=sorted(list(set(ly)))
+lz=sorted(list(set(lz)))
+res=0
+ON=set()
+mx=len(U)
+for iq,quad  in enumerate(U):
+	bx,by,bz,ex,ey,ez=quad
+	print(f"\r{iq+1}/{mx}",quad,end="")
+	for cx,cex in zip(lx,lx[1:]):
+		if bx<=cx<cex<=ex+1:
+			for cy,cey in zip(ly,ly[1:]):
+				if by<=cy<cey<=ey+1:
+					for cz,cez in zip(lz,lz[1:]):
+						if bz<=cz<cez<=ez+1:
+							ON.add((cx,cy,cz,cex,cey,cez))
+							# ~ res+=(ex-bx)*(ey-by)*(ez-cz)
+							# ~ res+=(cex-cx)*(cey-cy)*(cez-cz)
+						
+res=0
 # ~ for ON in UNIQ:
-	# ~ for cx,cy,cz,cex,cey,cez in ON:
-		# ~ res+=(cex-cx)*(cey-cy)*(cez-cz)
-# ~ repres (2,res)
+# ~ print(ON)
+for cx,cy,cz,cex,cey,cez in ON:
+	res+=(cex-cx)*(cey-cy)*(cez-cz)
+repres (2,res)
+print('expected 39769202357779 p0 ok')
