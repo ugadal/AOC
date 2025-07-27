807bd66ba479b7c75f00ffc8631f63d9607a045d
Ugadal <dcoorna@gmail.com>
2025-07-27 12:37:49 +0200

    up

--- 2021/Day 24: Arithmetic Logic Unit.py
+++ 2021/Day 24: Arithmetic Logic Unit.py
@@ -6,6 +6,7 @@ import re
 import sys
 import itertools as it
 from functools import cache
+import random
 # ~ import uuid
 # ~ import numpy as np
 cp=str(sys.argv[0])
@@ -13,7 +14,7 @@ dre=re.compile(r"^Day (\d+):")
 day=dre.findall(cp)[0]
 fn=f"d{day}.txt"
 # ~ block=open(fn).read().split("\n\n\n")[part]
-# ~ block=open(fn).read().split("\n\n")[part]
+block=open(fn).read().split("\n\n")[part]
 ops=[]
 class reg():
 	A={}
@@ -26,7 +27,7 @@ class reg():
 		reg.A[name]=self
 	def reset(self):
 		self.v=self.ori
-for line in open(fn).read().splitlines():
+for line in block.splitlines():
 	P=line.split()
 	if line.startswith("inp"):
 		tn=P[1]
@@ -45,18 +46,38 @@ for line in open(fn).read().splitlines():
 # ~ for r in reg.A.items():print(r)
 res=reg.A["z"]
 def run(pool):
+	# ~ P=[]
+	tp=list(pool)
 	for r in reg.A.values():r.reset()
 	for op,ta,tb in ops:
 		match op:
-			case 'inp':ta.v=pool.pop(0)
+			case 'inp':ta.v=tp.pop(0)
+			# ~ case 'inp':
+				# ~ ta.v=random.randrange(1,10)
+				# ~ P.append(ta.v)
 			case 'add':ta.v+=tb.v
+			case 'set':ta.v=tb.v
 			case 'mul':ta.v*=tb.v
-			case 'div':ta.v//=tb.v
-			case 'mod':ta.v%=tb.v
+			case 'div':
+				if tb.v==0:
+					print("err div",pool)
+					return False
+				ta.v//=tb.v
+			case 'mod':
+				if ta.v<0 or tb.v<0:
+					print("err mod")
+					return False
+				ta.v%=tb.v
 			case 'eql':ta.v=1 if ta.v==tb.v else 0
+			case 'dif':ta.v=1 if ta.v!=tb.v else 0
 	return res.v==0
+	# ~ else:
+		# ~ print(P,"wrong",res.v)
+		# ~ return False
 for pool in it.product([9,8,7,6,5,4,3,2,1],repeat=14):
-	print(pool)
+# ~ while True:
+	print("\r",pool,end="")
 	v=run(list(pool))
 	# ~ print(v)
-	if v:break
+	if v:
+		print(v)
