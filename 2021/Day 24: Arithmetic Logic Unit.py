f5f19017aa7e4a78aaa4aeb240981a26339390e6
Ugadal <dcoorna@gmail.com>
2025-07-27 08:35:41 +0200

    up

--- 2021/Day 24: Arithmetic Logic Unit.py
+++ 2021/Day 24: Arithmetic Logic Unit.py
@@ -15,10 +15,27 @@ fn=f"d{day}.txt"
 # ~ block=open(fn).read().split("\n\n\n")[part]
 # ~ block=open(fn).read().split("\n\n")[part]
 ops=[]
-
+class reg():
+	A={}
+	def __init__(self,name):
+		try:
+			v=int(name)
+			self.v=v
+		except:
+			self.v=0
+		reg.A[name]=self
 for line in open(fn).read().splitlines():
 	P=line.split()
-	if line.startswith("inp")
+	if line.startswith("inp"):
 		tn=P[1]
+		if tn in reg.A:tn=reg.A[tn]
+		else:tn=reg(tn)
 		ops.append(("inp",tn))
-		
+		continue
+	ta,tb=P[1:]
+	if ta in reg.A:ta=reg.A[ta]
+	else:ta=reg(ta)
+	if tb in reg.A:tb=reg.A[tb]
+	else:tb=reg(tb)
+	ops.append((P[0],ta,tb))
+print(ops)
