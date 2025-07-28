eb81125b256ac03a9ac91c83a3c6886ecd118bb3
Ugadal <dcoorna@gmail.com>
2025-07-28 19:34:38 +0200

    up

--- 2019/Day 24: Planet of Discord.py
+++ 2019/Day 24: Planet of Discord.py
@@ -16,34 +16,3 @@ day=dre.findall(cp)[0]
 fn=f"d{day}.txt"
 # ~ block=open(fn).read().split("\n\n\n")[part]
 block=open(fn).read().split("\n\n")[part]
-# ~ print(block)
-M=[computer(block) for x in range(50)]
-for ic,m in enumerate(M):
-	m.inp.append(ic)
-nat=[None,None,None,None]
-while True:
-	idle=True
-	for ic,m in enumerate(M):
-		while True:
-			x=next(m.flow)
-			if x=="waiting":break
-			idle=False
-			y=next(m.flow)
-			z=next(m.flow)
-			print(ic,x,y,z)
-			if x==255:
-				lastnat=(y,z)
-				# ~ if c==y and d==z:
-					# ~ print(nat)
-					# ~ exit()
-			else:
-				M[x].inp.extend((y,z))
-	if idle:
-		a,b,c,d=nat
-		y,z=lastnat
-		nat=[c,d,y,z]
-		if c==y and d==z:
-				print(nat[-1])
-				exit()
-		M[0].inp.extend((y,z))		
-				
