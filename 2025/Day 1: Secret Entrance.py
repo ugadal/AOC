83bec3cc238f5f34f5667762f5d652e595a5b691
David Coornaert <dcoorna@gmail.com>
2025-12-01 13:57:48 +0100

    up

--- 2025/Day 1: Secret Entrance.py
+++ 2025/Day 1: Secret Entrance.py
@@ -2,7 +2,7 @@
 #
 #  Day 1: Secret Entrance.py
 sep="\n\n"
-fn,part="d1.txt",0
+fn,part="d1.txt",1
 data=open(fn).read().split(sep)[part]
 res=50
 count=0
@@ -19,18 +19,13 @@ count=0
 
 for l in data.splitlines():
 	v=int(l[1:])
+	count+=v//100
+	v=v%100
 	match l[0]:
 		case 'L':res-=v
 		case 'R':res+=v
-	while res <=-100:
-		res+=100
-		count+=1
-	while res >=100:
-		res-=100
-		count+=1
-	# ~ if res==0:count+=1
-	# ~ elif oldres and res!=res%100:count+=1
-	# ~ res=res%100
-	# ~ oldres=res
-	# ~ print(l,res,count)
+	if res==0:count+=1
+	elif oldres and res!=res%100:count+=1
+	res=res%100
+	oldres=res
 print("p2 :",count)
