3336a66aa159e07c79bd3e18e3f524d8f503b74e
David Coornaert <dcoorna@gmail.com>
2025-12-01 08:10:08 +0100

    up

---
+++ 2025/Day 1: Secret Entrance.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+#
+#  Day 1: Secret Entrance.py
+sep="\n\n"
+fn,part="d1.txt",0
+data=open(fn).read().split(sep)[part]
+res=50
+count=0
+for l in data.splitlines():
+	v=int(l[1:])
+	match l[0]:
+		case 'L':res-=v
+		case 'R':res+=v
+	res=res%100
+	if res==0:count+=1
+print("p1 :",count)
+oldres=res=50
+count=0
+
+for l in data.splitlines():
+	v=int(l[1:])
+	match l[0]:
+		case 'L':res-=v
+		case 'R':res+=v
+	while res <=-100:
+		res+=100
+		count+=1
+	while res >=100:
+		res-=100
+		count+=1
+	# ~ if res==0:count+=1
+	# ~ elif oldres and res!=res%100:count+=1
+	# ~ res=res%100
+	# ~ oldres=res
+	# ~ print(l,res,count)
+print("p2 :",count)
