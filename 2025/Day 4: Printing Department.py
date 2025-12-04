b6c75f66f421d78501dee972e8922e9a68c2534f
Ugadal <dcoorna@gmail.com>
2025-12-04 08:09:10 +0100

    up

---
+++ 2025/Day 4: Printing Department.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+#
+fn,part="d4.txt",1
+sep="\n\n"
+data=open(fn).read().split(sep)[part]
+G={}
+for row,line in enumerate(data.splitlines()):
+	for col,s in enumerate(line):
+		pos=complex(col,row)
+		G[pos]=s
+R=row+1
+C=col+1
+def pmap(G):
+	for row in range(R):
+		print("".join([G[complex(col,row)] for col in range(C)]))
+def around(pos):
+	yield pos-1-1j
+	yield pos-1j
+	yield pos+1-1j
+	yield pos-1
+	yield pos+1
+	yield pos-1+1j
+	yield pos+1j
+	yield pos+1+1j
+res=0
+for row in range(R):
+	for col in range(C):
+		pos=complex(col,row)
+		if G.get(pos,".")==".":continue
+		a=[G.get(p,".") for p in around(pos)]
+		if a.count("@")<4:res+=1
+print("p1 :",res)
+cyc=0
+total=0
+while True:
+	pmap(G)
+	cyc+=1
+	torem=[]
+	for row in range(R):
+		for col in range(C):
+			pos=complex(col,row)
+			if G.get(pos,".")==".":continue
+			a=[G.get(p,".") for p in around(pos)]
+			if a.count("@")<4:torem.append(pos)
+	print(f"{cyc} : {len(torem)}")
+	for pos in torem:G[pos]="."
+	if not torem:break
+	total+=len(torem)
+print("p2 :",total)
