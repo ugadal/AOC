b999604d8f1f23b8e7cdb935a2b4158bd71198cf
Coornaert David PhD <dcoorna@gmail.com>
2025-12-08 10:46:38 +0100

    up

---
+++ 2025/Day 8: Playground.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+#
+from malib import *
+fn,part="d8.txt",1
+data=open(fn).read().split(sep)[part]
+
+JB=[tuple(map(int,l.split(","))) for l in data.splitlines()]
+
+def dist(i,j):
+	a,b,c=i
+	d,e,f=j
+	d=(a-d)**2+(b-e)**2+(c-f)**2
+	d=d**.5
+	return d
+rec=inf
+DP={}
+for i,j in it.combinations(JB,2):
+	d=dist(i,j)
+	DP[i,j]=d
+DL=sorted(DP.items(),key=lambda x:x[1])
+C=[]
+for (a,b),d in DL[:1000]:
+	if any(a in c for c in C):
+		ac=next(c for c in C if a in c)
+	else:ac=False
+	if any(b in c for c in C):
+		bc=next(c for c in C if b in c)
+	else:bc=False
+	if ac and bc:
+		if ac==bc:continue
+		C.remove(ac)
+		C.remove(bc)
+		C.append(ac|bc)
+		continue
+	if ac:
+		ac.add(b)
+		continue
+	if bc:
+		bc.add(a)
+		continue
+	C.append(set([a,b]))
+L=[len(c) for c in C]
+L.sort()
+res=reduce(L[-3:])
+print("p1 :",res)
+
+njb=len(JB)
+C=[]
+def checkconn(a,b,C):
+	if len(C)==1:
+		if len(C[0])==njb:
+			print("p2 :",a[0]*b[0])
+			exit()
+for (a,b),d in DL:
+	if any(a in c for c in C):
+		ac=next(c for c in C if a in c)
+	else:ac=False
+	if any(b in c for c in C):
+		bc=next(c for c in C if b in c)
+	else:bc=False
+	if ac and bc:
+		if ac==bc:continue
+		C.remove(ac)
+		C.remove(bc)
+		C.append(ac|bc)
+		checkconn(a,b,C)
+		continue
+	if ac:
+		ac.add(b)
+		checkconn(a,b,C)
+		continue
+	if bc:
+		bc.add(a)
+		checkconn(a,b,C)
+		continue
+	C.append(set([a,b]))
---
+++ 2025/malib.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+#
+import itertools as it
+sep="\n\n"
+inf=float("Inf")
+def readG(data):
+	G={}
+	for row,line in enumerate(data.splitlines()):
+		for col,s in enumerate(line):
+			pos=complex(col,row)
+			G[pos]=s
+	return G,row+1,col+1
+# ~ G,R,C=readG(data)
+def pmap(G):
+	for row in range(R):
+		print("".join([G[complex(col,row)] for col in range(C)]))
+def around(pos):
+	for dc in (-1,0,1):
+		for dr in (-1j,0,1j):
+			if dc+dr:yield pos+dc+dr
+def reduce(L):
+	p=1
+	for v in L:p=p*v
+	return p
