6b45ee1f9daa3646b274834ee285f1c3a14964c5
Ugadal <dcoorna@gmail.com>
2025-12-10 17:02:24 +0100

    up

--- 2025/Day 10: Factory.py
+++ 2025/Day 10: Factory.py
@@ -9,11 +9,7 @@ for line in data.splitlines():
 	target=parts[0]
 	buttons=parts[1:-1]
 	goal=[True if c=="#" else False for c in target[1:-1]]
-	# ~ print(target,goal)
-	# ~ buttons=[eval(b) for b in buttons]
-	# ~ print(buttons)
 	buttons=[list(map(int,b[1:-1].split(",")))  for b in buttons ]
-	# ~ print(buttons)
 	press=1
 	found=False
 	while not found:
@@ -31,3 +27,34 @@ for line in data.splitlines():
 		else:
 			press+=1
 print("p1 :",res)
+res=0
+for line in data.splitlines():
+	parts=line.split()
+	target=parts[-1]
+	buttons=parts[1:-1]
+	goal=list(map(int,target[1:-1].split(",")))
+	buttons=[list(map(int,b[1:-1].split(",")))  for b in buttons ]
+	pool=[]
+	for but in buttons:
+		mp=min(goal[v] for v in but)
+		pool.extend([but]*mp)
+	print(pool)
+	# ~ exit()
+	press=1
+	found=False
+	while not found:
+		print(press)
+		for co in it.combinations(pool,press):
+			# ~ print("comb",co)
+			status=[0]*len(goal)
+			for but in co:
+				for v in but:
+					status[v]+=1
+			if status==goal:
+				# ~ print("found",press)
+				res+=press
+				found=True
+				break
+		else:
+			press+=1
+print("p2 :",res)
